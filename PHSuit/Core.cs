8033ebf1f0b3723dc5d7d63e2dc8551e31a4a9ca
phiree <phiree@gmail.com>
2015-05-07 12:09:41 +0800

    Add Globle component

---
+++ PHSuit/Core.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PHSuit
+{
+    public class PHCore
+    {
+        public static DateTime GetNextDay(DateTime date)
+        {
+            DateTime nextDay =Convert.ToDateTime(date.AddDays(1).ToShortDateString());
+            return nextDay;
+        }
+        public static string GetRandom(int size)
+        { 
+            string max_str=string.Empty;
+            Random rm = new Random();
+            
+            for(int i=0;i<size;i++)
+            {
+                int c = rm.Next(9);
+                max_str+=""+c;
+            }
+
+            return max_str;
+            
+        }
+    }
+}
---
+++ PHSuit/test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PHSuit
+{
+     [TestFixture]
+   public class test
+    {
+         [Test]
+         public void test_GetNextDay()
+         {
+             DateTime dt = new DateTime(2009, 10, 2, 3, 10, 12);
+             DateTime nextday = PHCore.GetNextDay(dt);
+             Console.WriteLine(nextday.ToString());
+             Assert.AreEqual(nextday-new DateTime(2009, 10, 3),new TimeSpan(0));
+         }
+         [Test]
+         public void test_GetRandon()
+         {
+            string r= PHCore.GetRandom(10);
+            Console.WriteLine(r);
+            Assert.LessOrEqual(Convert.ToInt64(r), long.MaxValue);
+         }
+    }
+}
