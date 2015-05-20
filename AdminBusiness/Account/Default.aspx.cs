3b843ba75dae8d87ebafb9af6f71024c60f1059d
陈君宁 <66734079@qq.com>
2015-05-20 17:07:21 +0800

    加了一些页面
    加了一些页面

---
+++ AdminBusiness/Account/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dianzhu.Model;
+using Dianzhu.BLL;
+public partial class Account_Default : System.Web.UI.Page
+{
+    BLLBusiness bllBusiness = new BLLBusiness();
+    public string PageTile = "商家基本信息列表";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
