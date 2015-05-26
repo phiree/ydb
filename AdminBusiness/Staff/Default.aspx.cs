4640aa60356a1105ae6cce789b53a9e7d1925d74
phiree <phiree@gmail.com>
2015-05-26 17:38:15 +0800

    staff list

--- AdminBusiness/Staff/Default.aspx.cs
+++ AdminBusiness/Staff/Default.aspx.cs
@@ -12,13 +12,17 @@ public partial class Staff_Default : BasePage
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            BindList();
+        }
     }
     private void BindList()
     {
         int total=30;
         Business b = ((BusinessUser)CurrentUser).BelongTo;
         gvStaff.DataSource = bllStaff.GetList(b.Id, Guid.Empty, 0, 10, out total);
+        gvStaff.DataBind();
        
     }
 }
