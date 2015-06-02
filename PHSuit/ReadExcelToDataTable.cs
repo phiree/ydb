bb625b692047e2b7d040572ca2b675d9928e10f9
phiree <phiree@gmail.com>
2015-06-02 16:39:07 +0800

    service type import from excel

---
+++ PHSuit/ReadExcelToDataTable.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+using NPOI.SS.UserModel;
+namespace PHSuit
+{
+    /// <summary>
+    /// 通过表格形式的Excel流生成DataTable.
+    ///  第一行有效数据应该是 列名 类型.
+    ///  
+    /// </summary>
+    public class ReadExcelToDataTable
+    {
+
+        /// <summary>
+        /// 只创建表结构,不填充数据
+        /// </summary>
+        public bool OnlyCreateSchemar { get; set; }
+        /// <summary>
+        /// 从此行开始读取.
+        /// </summary>
+        public int RowNumberBegin { get; set; }
+        /// <summary>
+        /// 该文件内所有的图片
+        /// </summary>
+        public IList AllPictures { get; private set; }
+        /// <summary>
+        /// worksheet index, start with 0
+        /// </summary>
+        public int SheetIndex { get; set; }
+
+        Stream InputStream { get; set; } //excel 流
+
+
+        public ReadExcelToDataTable(Stream inputStream)
+        {
+            InputStream = inputStream;
+
+            OnlyCreateSchemar = false;
+            RowNumberBegin = 1;
+            SheetIndex = 0;
+        }
+        public ReadExcelToDataTable(Stream inputStream, bool needAllPictures
+            , bool onlyCreateSchemar, int rowNumberBegin)
+            : this(inputStream)
+        {
+
+            OnlyCreateSchemar = onlyCreateSchemar;
+            RowNumberBegin = rowNumberBegin;
+        }
+        public DataTable Read(out string errMsg)
+        {
+            StringBuilder sbErrMsg = new StringBuilder();
+            HSSFWorkbook book = new HSSFWorkbook(InputStream);
+
+            AllPictures = book.GetAllPictures();
+
+            var sheet = book.GetSheetAt(SheetIndex);
+            DataTable dt = new DataTable();
+            //起始行单元格内的值作为datatable的列名.
+            var row = sheet.GetRow(RowNumberBegin);
+            for (int i = 0; i < row.LastCellNum; i++)
+            {
+                var columnName = row.GetCell(i);
+                //空白列导致出错
+                string strColName = string.Empty;
+                if (columnName == null)
+                {
+                    strColName = Guid.NewGuid().ToString();
+                }
+                else
+                {
+                    strColName = columnName.ToString();
+                }
+                DataColumn col = new DataColumn(strColName, typeof(String));
+                dt.Columns.Add(col);
+            }
+            if (!OnlyCreateSchemar)
+            {
+                IEnumerator rowEnumer = sheet.GetRowEnumerator();
+                while (rowEnumer.MoveNext())
+                {
+
+                    var currentRow = (HSSFRow)rowEnumer.Current;
+                    if (currentRow.RowNum < RowNumberBegin + 1) continue;
+                    //防止其遍历到没有数据的row
+                    if (currentRow.LastCellNum < row.Cells.Count)
+                    {
+                        //    break;
+                    }
+                    //空白行判断:如果所有的cell都没有可见数据,则为空白行.
+                    bool isEnd = true;
+                    foreach (var cell in currentRow.Cells)
+                    {
+                        if (!string.IsNullOrEmpty(StringHelper.ReplaceSpace(cell.ToString())))
+                        {
+                            isEnd = false;
+                            break;
+                        }
+                    }
+                    if (isEnd)
+                    {
+
+                        break;
+                    }
+
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < row.LastCellNum; i++)
+                    {
+                        var cell = currentRow.GetCell(i);
+                        if (cell == null)
+                        {
+                            dr[i] = null;
+                        }
+                        else
+                        {
+                            dr[i] = cell.ToString();
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            errMsg = sbErrMsg.ToString();
+            return dt;
+        }
+
+
+
+
+    }
+}
---
+++ PHSuit/StringHelper.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace PHSuit
+{
+    public class StringHelper
+    {
+        public static string ReplaceSpace(string input)
+        {
+            string patern = @"\s*";
+            return Regex.Replace(input, patern, string.Empty);
+        }
+        public static string ReplaceInvalidChaInFileName(string input, string replacement)
+        {
+            input = input.Trim();
+            string partern = @"[\\\/\:\'\?\*\<\>\|\n|\s]";
+            return Regex.Replace(input, partern, replacement);
+        }
+        public static string ReplaceInvalidChaInFileName(string input)
+        {
+            return ReplaceInvalidChaInFileName(input, string.Empty);
+        }
+        public static bool ReplaceSpaceAndCompare(string s1, string s2)
+        {
+            s1 = ReplaceSpace(s1);
+            s2 = ReplaceSpace(s2);
+            return s1 == s2;
+        }
+        /// <summary>
+        /// 保证字符串的字符数量
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="exceptLenth">希望的长度</param>
+        /// <param name="fillChar"> 填充字符 </param>
+        /// <returns></returns>
+        public static string EnsureStringLength(string input, int exceptLenth, char fillChar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string[] Split(string original, char s)
+        {
+            return original.Split(s);
+        }
+        /// <summary>
+        /// 通过普通的Query hql语句 获取 count* 语句
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static string BuildCountQuery(string query)
+        {
+            ////"select s from supplier from supplier d where 1=1   "
+
+
+            Regex reg = new Regex(@"(?<=select\s+(distinct)?).*?(?=from)");
+
+            Match m = reg.Match(query);
+
+            string result = reg.Replace(query, " count(*) ", 1);
+            return result;
+        }
+        #region 全角半角转换
+
+        /// <summary>
+
+        /// 转全角的函数(SBC case)
+
+        /// </summary>
+
+        /// <param name="input">任意字符串</param>
+
+        /// <returns>全角字符串</returns>
+
+        ///<remarks>
+        ///</remarks>
+
+        public static string ToSBC(string input)
+        {
+
+            //半角转全角：
+
+            char[] c = input.ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+
+                if (c[i] == 32)
+                {
+
+                    c[i] = (char)12288;
+
+                    continue;
+
+                }
+
+                if (c[i] < 127)
+
+                    c[i] = (char)(c[i] + 65248);
+
+            }
+
+            return new string(c);
+
+        }
+
+        /// <summary> 转半角的函数(DBC case) </summary>
+
+        /// <param name="input">任意字符串</param>
+
+        /// <returns>半角字符串</returns>
+
+        ///<remarks>
+
+        ///全角空格为12288，半角空格为32
+
+        ///其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+
+        ///</remarks>
+
+        public static string ToDBC(string input)
+        {
+
+            char[] c = input.ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+
+                if (c[i] == 12288)
+                {
+
+                    c[i] = (char)32;
+
+                    continue;
+
+                }
+
+                if (c[i] > 65280 && c[i] < 65375)
+
+                    c[i] = (char)(c[i] - 65248);
+
+            }
+
+            return new string(c);
+
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 用正则判断字符串属于哪种语言.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string LanguageTypeDetermine(string input)
+        {
+            if (Regex.IsMatch(input, "[\u4e00-\u9fa5]"))
+            {
+                return "zh";
+            }
+            return "en";
+        }
+        /// <summary>
+        /// 补全字符串
+        /// </summary>
+        /// <param name="original"></param>
+        /// <returns></returns>
+        public static string FullFillWidth(string original, string fillString, int width, bool prepend)
+        {
+            string s = fillString + original;
+            if (!prepend)
+                s = original + fillString;
+            return s.Substring(s.Length - width);
+        }
+
+
+    }
+}
