f9ae45d8ee5c28010ebe52a536f907d7c90309a3
phiree <phiree@gmail.com>
2015-06-29 18:17:42 +0800

    打开服务列表时, 自动加载最后一次修改的服务信息.

--- Dianzhu.BLL/BLLDZService.cs
+++ Dianzhu.BLL/BLLDZService.cs
@@ -7,30 +7,37 @@ using Dianzhu.Model;
 using Dianzhu.DAL;
 namespace Dianzhu.BLL
 {
-  public  class BLLDZService
+    public class BLLDZService
     {
-      IDALDZService iDALDZService;
+        DALDZService iDALDZService;
 
-      public BLLDZService(IDALDZService iDALDZService)
-      {
-          this.iDALDZService = iDALDZService;
-      }
-      public BLLDZService()
-          : this(new DALDZService())
-      { }
+        public BLLDZService(DALDZService iDALDZService)
+        {
+            this.iDALDZService = iDALDZService;
+        }
+        public BLLDZService()
+            : this(new DALDZService())
+        { }
 
-      public IList<DZService> GetServiceByBusiness(Guid businessId,Guid serviceTypeId,int pageindex,int pagesize,out int totalRecords)
-      {
-          return iDALDZService.GetList(businessId, serviceTypeId, pageindex, pagesize, out totalRecords);
-      }
-      public DZService GetOne(Guid serviceId)
-      {
-          return iDALDZService.DalBase.GetOne(serviceId);
-      }
-      public void SaveOrUpdate(DZService service)
-      {
-          iDALDZService.DalBase.SaveOrUpdate(service);
-      }
-       
+        public IList<DZService> GetServiceByBusiness(Guid businessId, Guid serviceTypeId, int pageindex, int pagesize, out int totalRecords)
+        {
+            return iDALDZService.GetList(businessId, serviceTypeId, pageindex, pagesize, out totalRecords);
+        }
+        
+        public DZService GetOne(Guid serviceId)
+        {
+            return iDALDZService.DalBase.GetOne(serviceId);
+        }
+        public void SaveOrUpdate(DZService service)
+        {
+            if (service.Id == Guid.Empty)
+            {
+                service.CreatedTime = DateTime.Now;
+            }
+
+            service.LastModifiedTime = DateTime.Now;
+            iDALDZService.DalBase.SaveOrUpdate(service);
+        }
+
     }
 }
