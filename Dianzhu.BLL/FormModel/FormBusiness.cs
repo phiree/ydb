60b034dc9219c05557a765c987515d4a2c1294fb
phiree <phiree@gmail.com>
2015-07-03 18:37:02 +0800

    thinking about  django form to asp,net webform.

---
+++ Dianzhu.BLL/FormModel/FormBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.Model;
+namespace Dianzhu.BLL.FormModel
+{
+    public class FormBusiness
+    {
+        private Business Business { get; set; }
+        public string AddressInfo { get; set; }
+
+    }
+}
