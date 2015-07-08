1471f85b3d4095016e33aa3e6392e90bab4bf413
licdream <licdream@126.com>
2015-07-08 15:15:19 +0800

    简单粗暴的解决3G版服务地图加载时，初始化定位点问题。

--- AdminBusiness/m/js/getMySopMap.js
+++ AdminBusiness/m/js/getMySopMap.js
@@ -1,53 +1,28 @@
-//$(document).ready(function(){
+$(document).ready(function(){
 
 
 // 百度地图API功能
     var map = new BMap.Map("container");
-//    var myCityListObject = new BMapLib.CityList({container : "city-container" , map : map });
-    //map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
-
     map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
     var myCityListObject = new BMapLib.CityList({
         container : "city-container",
         map : map
     });
     console.log(map.getCenter());
-  //  map.centerAndZoom("海口", 11);     // 初始化地图,设置中心点坐标和地图级别
     map.enableScrollWheelZoom();
     map.clearOverlays();    //清除地图上所有覆盖物
+
     map.addEventListener("tilesloaded", function (e) {
         //  var point = new BMap.Point(116.404, 39.915);
         // var marker = new BMap.Marker(point);
         //   map.addOverlay(marker);
         //$("#businessID-button").css("display","none");
-
         $("#city-container .ui-select:eq(3)").css("display", "none");
       //  $("#city-container .ui-select:eq(3)").p
 
-
-
-
     });
-  //  alert($('#hiAddrId').attr("value"));
-    if ($('#hiAddrId').attr("value")) {
-        var readAddrJson = jQuery.parseJSON($('#hiAddrId').attr("value"));
-        var addressNode = readAddrJson.province + readAddrJson.city + readAddrJson.district + readAddrJson.street + readAddrJson.streetNumber
-        $("#serArea-txt").html(addressNode);
-        $(".showAdd").html(addressNode);
-        var vpoint = new BMap.Point(readAddrJson.lng, readAddrJson.lat)
-         console.log(vpoint);
-       //var vpoint = new BMap.Point(110, 30)
-        //map.centerAndZoom(readAddrJson.city, 12);     // 初始化地图,设置中心点坐标和地图级别
-        map.centerAndZoom(vpoint, 12);     // 初始化地图,设置中心点坐标和地图级别
-        // map.panTo(vpoint);
-        // console.log(map);
-       //console.log(vpoint)
-        var rmarker = new BMap.Marker(vpoint); // 创建点
-        map.addOverlay(rmarker);    //增加点
 
-    }
-       
-    (function(){
+    function defaultSetPoint (){
         if ($('#hiAddrId').attr("value")) {
             var readAddrJson = jQuery.parseJSON($('#hiAddrId').attr("value"));
             var addressNode = readAddrJson.province + readAddrJson.city + readAddrJson.district + readAddrJson.street + readAddrJson.streetNumber;
@@ -57,21 +32,17 @@
             $("#serArea-txt").html(addressNode);
             $(".showAdd").html(addressNode);
 
-            map.panTo(vPoint);     // 初始化地图,设置中心点坐标和地图级别
             map.setCenter(vPoint);     // 初始化地图,设置中心点坐标和地图级别
             map.setZoom(12);
             console.log(vPoint);
             console.log(map.getCenter());
             map.addOverlay(rMarker);    //增加点
         }
-    })();
+    }
+    setTimeout(defaultSetPoint, 500); //简单粗暴的通过延时解决页面初始化时，中心点被CityList的DataMgr初始覆盖;
 
     var gc = new BMap.Geocoder(); //地址解析类
     function showInfo(e) {
-        //alert(e.point.lng + ", " + e.point.lat);
-
-        //alert(allOverlay.length-1)
-//        deletePoint();
         map.clearOverlays();
         var point = new BMap.Point(e.point.lng, e.point.lat);
         var marker = new BMap.Marker(point); // 创建点
@@ -96,7 +67,6 @@
 //            var lngLat = e.point.lng + "," + e.point.lat;
 
           //  $("#lngLat").val(lngLat);//经度与纬度
-           
             $(".showAdd").html(mapAddr);//地点
 
         });
@@ -109,27 +79,6 @@
 //        map.clearOverlays();
 //    }
 
-    map.addEventListener("click", showInfo); 
-    //百度地图自动填充关键词
-//    function G(id) {
-//        return document.getElementById(id);
-//    }
+    map.addEventListener("click", showInfo);
 
-   
-
-    function setPlace(){
-        map.clearOverlays();    //清除地图上所有覆盖物
-        function myFun(){
-            var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
-            map.centerAndZoom(pp, 18);
-            map.addOverlay(new BMap.Marker(pp));    //添加标注
-
-        }
-        var local = new BMap.LocalSearch(map, { //智能搜索
-            onSearchComplete: myFun
-        });
-        local.search(myValue);
-    }
-
-
-//});
+});
