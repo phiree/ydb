d1822eecc81a2577cc928c2708e8194c3a78216d
licdream <licdream@126.com>
2015-07-14 19:22:38 +0800

    修改注册步骤，合二为一。添加验证用户名是否存在方法。重写验证方法。

--- AdminBusiness/js/login_reg.js
+++ AdminBusiness/js/login_reg.js
@@ -1,223 +1,212 @@
 $(document).ready(function () {
-    //验证图标显示控制
+    var default_user_text = "手机号码/电子邮箱";
 
-    $("#tbxUserName").InlineTip({ 'tip': "手机号码/电子邮箱" });
+    $("#tbxUserName").InlineTip({ 'tip': default_user_text });
 
-    var chkIconAnm = function (hide, jdg, icon) {
-        if (!hide) {
-            if (!jdg) {  //条件不符合
-                icon.removeClass('chkRight').addClass('chkError');
-            } else {  //条件符合
-                icon.removeClass('chkError').addClass('chkRight');
+    var regCheck = function(){
+
+        var $user_name_box = $(".regUserName");
+        var user_name_rule = /^1\d{10}$|^.+@.+\..+$/;
+        var user_warn_element = document.createElement("div");
+        var user_warn_text = document.createTextNode("");
+
+        var user_right = false,
+            pass_right = false,
+            pass_conf_right = false,
+            agree_right = false;
+
+        $(user_warn_element).addClass("err_msg");
+        $user_name_box.parent().append(user_warn_element);
+        $(user_warn_element).hide();
+
+
+        var userNameCheck = function(){
+            var _user_name = $user_name_box.val();
+            var $_checkIcon = $user_name_box.parent().find(".checkIcon");
+
+
+            if ( _user_name == null || _user_name == default_user_text ){
+                chkIconAnm(false, false, $_checkIcon);
+                $(user_warn_element).show();
+                user_warn_text = "请填写用户名";
+                $(user_warn_element).text(user_warn_text);
+            } else if ( !_user_name.match(user_name_rule) ) {
+                $(user_warn_element).show();
+                chkIconAnm(false, false, $_checkIcon);
+                user_warn_text = "格式错误";
+                $(user_warn_element).text(user_warn_text);
+            } else if ( userNameDupChk(_user_name) == "error" ) {
+                $(user_warn_element).show();
+                chkIconAnm(false, false, $_checkIcon);
+                user_warn_text = "请求出错";
+                $(user_warn_element).text(user_warn_text);
+            } else if( userNameDupChk(_user_name) ){
+                $(user_warn_element).show();
+                chkIconAnm(false, false, $_checkIcon);
+                user_warn_text = "用户名已存在";
+                $(user_warn_element).text(user_warn_text);
+            } else {
+                user_right = true;
+                chkIconAnm(false, true, $_checkIcon);
+                $(user_warn_element).hide();
             }
-        } else {
-            icon.removeClass('chkError').removeClass('chkRight');
-        }
-    };
+        };
 
-    //验证用户是否存在
-//    var showCheck = function (objInput, icon) {
-//        objInput.blur(function () {
-//            var val = objInput.val();
-//            var transData = { username: val };
-//
-//            if (objInput.val() != '') {
-//                $.ajax({
-//                    type: 'GET',
-//                    async: false,
-//                    url: "/AjaxService/is_username_duplicate.ashx",
-//                    data: transData,
-//
-//                    //            jsonp:"callback",
-//                    success: function (json) {//后台返回json,json=1用户不存在,json=0用户存在
-//                        if (json == "N") json = 1;
-//                        else if (json == "Y") json = 0;
-//                        chkIconAnm(false, json, icon);
-//                    },
-//                    error: function (e) {
-//                        alert('error!'+e);
-//                    }
-//                });
-//            } else {
-//                chkIconAnm(true, false, icon)
-//                return false;
-//            }
-//        });
-//    };
-//    showCheck($('#tbxUserName'), $('#phoneCheck'));
+        var userNameDupChk = function(username){
 
-    //密码验证
-    var passCheck = function (psw, pswConf, iconChk, iconConfChk) {
-        var passRule = /^[A-Za-z0-9_-]+$/;
+            var dup_ajax = $.ajax({
+                type: "get",
+                url: "/ajaxservice/is_username_duplicate.ashx?username=" + username,
+                async: false
+            });
+
+            if ( dup_ajax.status == 200 ){
+                var _dup = dup_ajax.responseText == "Y" ? true : false;
+                return _dup;
+            } else {
+                return "error";
+            }
+
+        };
+
+        $user_name_box.blur(userNameCheck);
+
+        var pass_rule = /^[A-Za-z0-9_-]+$/;
+        var $pass_box = $('#regPs');
+        var $pass_conf_box = $('#regPsConf');
+        var pass_warn_element = document.createElement("div");
+        var pass_warn_text = document.createTextNode("");
+        var pass_conf_warn_element = document.createElement("div");
+        var pass_conf_warn_text = document.createTextNode("");
         var ruleR;
 
+        $(pass_warn_element).addClass("err_msg");
+        $(pass_conf_warn_element).addClass("err_msg");
+        $pass_box.parent().append(pass_warn_element);
+        $pass_conf_box.parent().append(pass_conf_warn_element);
+        $(pass_warn_element).hide();
+        $(pass_conf_warn_element).hide();
 
-        psw.bind('input propertychange',function(){ //密码是否符合规则
-            if ( !psw.val() ){
-                chkIconAnm(true, false , iconChk);
-                $('#passCheckText').hide();
-                ruleR = false;
+        var passCheck = function (e) {
+            var eve = e || window.event;
+            var _pass_value = $pass_box.val();
+            var $_checkIcon = $pass_box.parent().find(".checkIcon");
+            if ( !_pass_value ){
+                if ( eve.type == "input" || eve.type == "propertychange" ){
+                    chkIconAnm(true, false , $_checkIcon);
+                    $(pass_warn_element).hide();
+                } else {
+                    chkIconAnm(false, false , $_checkIcon);
+                    $(pass_warn_element).show();
+                    pass_warn_text = "密码不符合要求，要求6-20个字符";
+                    $(pass_warn_element).text(pass_warn_text);
+                    ruleR = false;
+                }
             } else {
-                if (( psw.val().length >= 6 ) && ( psw.val().length <= 20 ) && passRule.test( psw.val() ) ) {
-                    chkIconAnm(false, true, iconChk);
-                    $('#passCheckText').hide();
+                if (( _pass_value.length >= 6 ) && ( _pass_value.length <= 20 ) && pass_rule.test( _pass_value ) ) {
+                    pass_right = true;
+                    chkIconAnm(false, true, $_checkIcon);
+                    $(pass_warn_element).hide();
                     ruleR = true;
                 } else {
-                    chkIconAnm(false, false, iconChk);
-                    $('#passCheckText').show();
+                    chkIconAnm(false, false, $_checkIcon);
+                    $(pass_warn_element).show();
+                    pass_warn_text = "密码不符合要求，要求6-20个字符";
+                    $(pass_warn_element).text(pass_warn_text);
                     ruleR = false;
                 }
             }
-        });
+        };
 
-        pswConf.bind('input propertychange',function(){ //密码确认是否一致
+
+        var passConfCheck = function(){
+            var _pass_conf_value = $pass_conf_box.val();
+            var $_checkIcon = $pass_conf_box.parent().find(".checkIcon");
+
+
             if ( !ruleR ) {
-                chkIconAnm(true, false, iconConfChk);
+                chkIconAnm(true, false, $_checkIcon);
             } else {
-                if (pswConf.val() == psw.val()) {
-                    chkIconAnm(false, true, iconConfChk);
-                    $('#passConfText').hide();
-                    $('#regPsSubmit').attr("onclick","return true;");
+                if (_pass_conf_value == $pass_box.val()) {
+                    pass_conf_right = true;
+                    chkIconAnm(false, true, $_checkIcon);
+                    $(pass_conf_warn_element).hide();
                 } else {
-                    chkIconAnm(false, false, iconConfChk);
-                    $('#passConfText').show();
-                    $('#regPsSubmit').attr("onclick","return false;");
+                    chkIconAnm(false, false, $_checkIcon);
+                    $(pass_conf_warn_element).show();
+                    pass_conf_warn_text = "两次密码不一致";
+                    $(pass_conf_warn_element).text(pass_conf_warn_text);
                 }
+            }
+        };
+
+        $pass_box.bind('input propertychange', passCheck);
+        $pass_conf_box.bind('input propertychange', passConfCheck);
+
+        var $agree_LIC =  $('input[name="agreeLic"]:checkbox');
+        if ( $agree_LIC.get(0) ) {
+            $agree_LIC.get(0).checked = true;
+            agree_right = true;
+        }
+
+        var agree_warn_element = document.createElement("div");
+        var agree_warn_text = document.createTextNode("");
+
+        $(agree_warn_element).addClass("err_msg");
+        $agree_LIC.parent().append(agree_warn_element);
+        $(agree_warn_element).hide();
+
+        var agreeCheck = function() {
+            //console.log($agree_LIC.get(0).checked);
+            if (!$agree_LIC.get(0).checked) {
+                agree_warn_text = "请仔细阅读协议，同意协议条例方可可注册。";
+                $(agree_warn_element).text(agree_warn_text);
+                $(agree_warn_element).show();
+                agree_right = false;
+            } else {
+                $(agree_warn_element).hide();
+                agree_right = true;
+            }
+        };
+
+        $agree_LIC.bind("click",agreeCheck);
+
+
+
+        var $reg_submit = $('#regPsSubmit');
+
+        $reg_submit.click(function(eve){
+            var e = eve || window.event;
+            userNameCheck();
+            passCheck();
+            passConfCheck();
+            agreeCheck();
+            if ( user_right && pass_right && pass_conf_right && agree_right ) {
+                return true;
+            } else {
+                e.preventDefault();
             }
         });
 
+        var chkIconAnm = function (hide, jdg, icon) {
+            var _hide = hide;// 隐藏标志
+            var _judge = jdg;// 是否符合条件
+            var $_icon = icon;// 图标对象
+
+
+            if (!_hide) {
+                if (!_judge) {  //条件不符合
+                    $_icon.removeClass('chkRight').addClass('chkError');
+                } else {  //条件符合
+                    $_icon.removeClass('chkError').addClass('chkRight');
+                }
+            } else {
+                $_icon.removeClass('chkError').removeClass('chkRight');
+            }
+        };
 
     };
-    passCheck($('#regPs'), $('#regPsConf'), $('#psChk'), $('#psConfChk'));
 
-    var regCheck = function(){
-        var userName = $("#tbxUserName");
-
-        var userNameRule = /^1\d{10}$|^.+@.+\..+$/;
-
-        var agreeLic =  $('input[name="agreeLic"]:checked');
-
-        var userNameCheck = function(){
-            
-            if ( $("#tbxUserName").val() == null || $("#tbxUserName").val() == "手机号码/电子邮箱" ){
-                $('#userCheck').addClass('chkError');
-                $("#userCheckText").text("请填写用户名");
-            } else if ( !$("#tbxUserName").val().match(regFilter) ) {
-                $('#userCheck').addClass('chkError');
-                $("#userCheckText").text("格式错误");
-            } else if ( !agreeLic.val() ) {
-                $('#userCheck').addClass('chkError');
-
-                $("#userCheckText").text("请同意协议");
-            } else {
-                $('#usernameConf').text($("#tbxUserName").val());
-                $('#userCheck').hide();
-                $("#userCheckText").text("");
-                $('.main-reg').toggle();
-                $('.main-psw').toggle();
-            }
-        }
-    }
-
-
-    //注册名选择方法
-
-//    var selectMeth = function () {
-//        //        alert($('#regMeth').val());
-//        var resetMeth = function (objInput, icon) {
-//            objInput.val(null);
-//            chkIconAnm(true, false, icon);
-//        };
-//
-//        if ($('#regMeth').val() == '1') {
-//            resetMeth($('#email'), $('#emailCheck'));
-//            $('.methText').text('输入号码');
-//            $('.emailBox').hide();
-//            $('.phoneBox').show();
-//        } else if ($('#regMeth').val() == '2') {
-//            //            alert($('#regMeth').val());
-//            resetMeth($('#phone'), $('#phoneCheck'));
-//            $('.methText').text('输入邮箱');
-//            $('.phoneBox').hide();
-//            $('.emailBox').show();
-//        } else {
-//            return null;
-//        }
-//    }
-
-    //用户名验证
-
-    var regFilter = /^1\d{10}$|^.+@.+\..+$/;
-    var regChang = function () {
-//        var $regForm1 = $('#regFormUser');
-//        var $regForm2 = $('#regFormPass');
-
-        // $regForm1.submit
-        $("#userConfirm").bind("click",
-            function () {
-//                console.log($("#tbxUserName").val().match(regFilter));
-                var agreeLic =  $('input[name="agreeLic"]:checked');
-//                console.log(agreeLic.val());
-                if ( $("#tbxUserName").val() == null || $("#tbxUserName").val() == "手机号码/电子邮箱" ){
-                    $('#userCheck').addClass('chkError');
-                    $("#userCheckText").text("请填写用户名");
-                } else if ( !$("#tbxUserName").val().match(regFilter) ) {
-                    $('#userCheck').addClass('chkError');
-                    $("#userCheckText").text("格式错误");
-                } else if ( !agreeLic.val() ) {
-                    $('#userCheck').addClass('chkError');
-
-                    $("#userCheckText").text("请同意协议");
-                } else {
-                    $('#usernameConf').text($("#tbxUserName").val());
-                    $('#userCheck').hide();
-                    $("#userCheckText").text("");
-                    $('.main-reg').toggle();
-                    $('.main-psw').toggle();
-                }
-            }
-        )
-        //$("#userConfirmBack").bind("click",
-        //    function(){
-        //        $('.main-reg').toggle();
-        //        $('.main-psw').toggle();
-        //    }
-        //)
-    }
-    regChang();
-
-
-    //var regOrLogin = function () {
-    //    $('.doReg').click(
-    //        function () {
-    //            return;
-    //            $('.wrap-login').animate(
-    //                {
-    //                    top: -50 + '%'
-    //                },
-    //                1000, false);
-    //            $('.wrap-reg').animate(
-    //                {
-    //                    top: 50 + '%'
-    //                },
-    //                1000, false)
-    //        });
-    //
-    //    $('.doLogin').click(
-    //        function () {
-    //            return;
-    //            $('.wrap-login').animate(
-    //                {
-    //                    top: 50 + '%'
-    //                },
-    //                1000, false);
-    //            $('.wrap-reg').animate(
-    //                {
-    //                    top: 150 + '%'
-    //                },
-    //                1000, false)
-    //        });
-    //}
-    //regOrLogin();
+    regCheck();
 
 });
--- AdminBusiness/register.aspx
+++ AdminBusiness/register.aspx
@@ -37,30 +37,28 @@
                                 <p>确认密码</p>
                             </div>
                             <div class="box-r">
-                                <div class="emailBox m-b10">
-                                    <asp:TextBox runat="server" ID="tbxUserName" ValidationGroup="vg_UserName" ClientIDMode="Static"></asp:TextBox>
-                                     <i id="userCheck" class="checkIcon"></i><span id="userCheckText" class="regWarnTexti"></span>
+                                <div class="reg-input m-b10">
+                                    <asp:TextBox runat="server" CssClass="regUserName" ID="tbxUserName" ValidationGroup="vg_UserName" ClientIDMode="Static"></asp:TextBox>
+                                    <i class="checkIcon"></i>
                                 </div>
-                                <!--<p class="rPage" id="usernameConf"></p>-->
-                                <div class="regPs">
+                                <div class="reg-input">
                                     <asp:TextBox runat="server" ClientIDMode="Static" ID="regPs" TextMode="Password"></asp:TextBox>
-                                    <i id="psChk" class="checkIcon"></i>
-                                    <div id="passCheckText" class="regWarnText dis-n">密码不符合要求，要求6-20个字符</div>
+                                    <i class="checkIcon"></i>
                                 </div>
-                                <div class="regPsConf">
+                                <div class="reg-input">
                                     <asp:TextBox runat="server" ClientIDMode="Static" ID="regPsConf" TextMode="Password"></asp:TextBox>
-                                    <i id="psConfChk" class="checkIcon"></i>
-                                    <div id="passConfText" class="regWarnText dis-n">两次密码不一致</div>
+                                    <i class="checkIcon"></i>
                                 </div>
-                                <p class="agree">
+                                <div class="agree">
                                     <input id="agreeLic" name="agreeLic" type="checkbox" value="yes" /><label class="v-m m-l10" for="agreeLic">我已经仔细阅读过《<a
-                                        class="agreeLIC-a" href="#">点助服务协议</a>》，并同意所有条款。</label></p>
+                                        class="agreeLIC-a" href="#">点助服务协议</a>》，并同意所有条款。</label></div>
                                 <!--<div class="buttonBox">-->
                                     <!--<input id="userConfirm"  type="button" class="regBtn"/>-->
                                 <!--</div>-->
                                 <div class="buttonBox">
-                                    <!--<input type="button" id="userConfirmBack" class="userConfirm-Back"/>-->
-                                    <asp:Button runat="server" ID="regPsSubmit" onClientClick="javascipt:return false;" ClientIDMode="Static" OnClick="regPsSubmit_OnClick"
+                                    <!--<input type="button" id="userConfirmBack"  class="userConfirm-Back"/>-->
+                                    <!--onClientClick="javascipt:return false;"-->
+                                    <asp:Button runat="server" ID="regPsSubmit"  ClientIDMode="Static" OnClick="regPsSubmit_OnClick"
                                         CssClass="regBtn" />
                                 </div>
                                 <p class="doLogin">
