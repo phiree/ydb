cfca35b5b6d4fe4681899537a74395d9fd18a104
phiree <phiree@gmail.com>
2015-07-24 10:36:47 +0800

    xmpp Winform

--- Dianzhu.XmppClient/Program.cs
+++ Dianzhu.XmppClient/Program.cs
@@ -15,7 +15,7 @@ namespace Dianzhu.XmppClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FmLogin());
         }
     }
 }
