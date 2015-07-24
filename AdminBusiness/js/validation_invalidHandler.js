41f9917ab07bb1704dc75be7822de3cbcd2de8a0
phiree <phiree@gmail.com>
2015-07-24 18:02:19 +0800

    前台验证 jquery validate 样例完成,

---
+++ AdminBusiness/js/validation_invalidHandler.js
@@ -0,0 +1,15 @@
+﻿var invalidHandler = function (form, validator) {
+    var errors = validator.numberOfInvalids();
+    if (errors) {
+        if ($(validator.errorList[0].element).is(":visible")) {
+            $('html, body').animate({
+                scrollTop: $(validator.errorList[0].element).offset().top
+            }, 1000);
+        }
+        else {
+            $('html, body').animate({
+                scrollTop: $("#" + $(validator.errorList[0].element).attr("focusID")).offset().top
+            }, 1000);
+        } //else
+    } //if(errors)
+}
---
+++ AdminBusiness/js/validation_service_edit.js
@@ -0,0 +1,40 @@
+﻿var service_validate_rules =
+                            {
+                                    ctl00$ContentPlaceHolder1$ctl00$tbxName:
+                                    {
+                                        required: true,
+                                        maxlength: 100
+
+                                    },
+                                    ctl00$ContentPlaceHolder1$ctl00$hiTypeId:
+                                    {
+                                            required: true,
+                                    },
+                                    ctl00$ContentPlaceHolder1$ctl00$tbxMinPrice:
+                                    {
+                                    
+                                    }
+                                     
+                                };//rule
+var service_validate_messages={
+    
+                                   ctl00$ContentPlaceHolder1$ctl00$tbxName:
+                                    {
+                                        required: "请填写服务名称",
+                                        maxlength: "不能超过100个字符"
+
+                                    },
+                                    ctl00$ContentPlaceHolder1$ctl00$hiTypeId:
+                                    {
+                                            required: "请选择服务类型",
+                                    }
+                                    ,
+                                    ctl00$ContentPlaceHolder1$ctl00$tbxMinPrice:
+                                    {
+                                    required:"请填写最低服务价格"
+                                    }
+
+                                
+};
+
+
