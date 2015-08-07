c69b61af2cd8a1e319ca98cc1cdc51e291924700
phiree <phiree@gmail.com>
2015-08-08 00:11:53 +0800

    客服系统--通讯模块--请求池可以接收请求了.

--- Dianzhu.XMLLPool/GlobalViables.cs
+++ Dianzhu.XMLLPool/GlobalViables.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using agsXMPP;
-namespace Dianzhu.Xmpp
+namespace Dianzhu.CSClient
 {
     
     public class GlobalViables
     {
+        public static readonly string ServerName = "yuanfei-pc";
         public static XmppClientConnection XMPPConnection = new XmppClientConnection();
     }
 }
