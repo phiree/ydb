e4e6d6f1847d77bdb4590ac10bd74c1c93bb049f
phiree <phiree@gmail.com>
2015-08-11 16:00:00 +0800

    通讯模块:通过ip连接.

--- Dianzhu.CSClient/GlobalViables.cs
+++ Dianzhu.CSClient/GlobalViables.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using agsXMPP;
+using System.Configuration;
 namespace Dianzhu.CSClient
 {
     
     public class GlobalViables
     {
-        public static readonly string ServerName = "yuanfei-pc";
+        public static readonly string ServerName = ConfigurationManager.AppSettings["server"];
         public static XmppClientConnection XMPPConnection = null;
         static GlobalViables()
         {
+            
             XMPPConnection = new XmppClientConnection(ServerName);
         }
         public static string CurrentUserName = string.Empty;
