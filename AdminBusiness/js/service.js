5c224c6d46d5f1a24ed0258e32366da1242f1cf8
licdream <licdream@126.com>
2015-08-13 21:48:35 +0800

    map at service_edit.aspx add controls ,disable mousewheel zoom.

--- AdminBusiness/js/service.js
+++ AdminBusiness/js/service.js
@@ -8,9 +8,23 @@ function initializeService(){
     //var map = new BMap.Map("businessMap");
     //var cityListObject = new BMapLib.CityList({ container: "businessCity" , map : map });
     map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
-    map.enableScrollWheelZoom();
     map.disableDoubleClickZoom();
+    map.disableScrollWheelZoom();
 
+    var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+    var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+    //var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，仅包含平移和缩放按钮
+    /*缩放控件type有四种类型:
+     BMAP_NAVIGATION_CONTROL_SMALL：仅包含平移和缩放按钮；BMAP_NAVIGATION_CONTROL_PAN:仅包含平移按钮；BMAP_NAVIGATION_CONTROL_ZOOM：仅包含缩放按钮*/
+
+    //添加控件和比例尺
+    function add_control(){
+        map.addControl(top_left_control);
+        map.addControl(top_left_navigation);
+        //map.addControl(top_right_navigation);
+    }
+
+    add_control();
 
 
     /**
