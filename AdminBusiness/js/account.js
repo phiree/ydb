352a4da9284c7673dfdc92faa83ce45d4cc3bf99
licdream <licdream@126.com>
2015-08-20 18:48:20 +0800

    店铺编辑页面：店铺地址当无发解析时，默认使用IP定位的地址解析做商圈范围。

--- AdminBusiness/js/account.js
+++ AdminBusiness/js/account.js
@@ -1,8 +1,8 @@
 function initialize(){
     var map = new BMap.Map("addressMap");
-    var cityListObject = new BMapLib.CityList({ container: "addressCity", map : map});
+    //var cityListObject = new BMapLib.CityList({ container: "addressCity", map : map});
     var geoc = new BMap.Geocoder();
-//var myGeo = new BMap.Geocoder();
+    var myCity = new BMap.LocalCity();
 
     var id_prefix = 'ContentPlaceHolder1_ContentPlaceHolder1_';
     var addressId = id_prefix + "tbxAddress";
@@ -23,18 +23,20 @@ function initialize(){
         var strAddress = _this.value;
 
         geoc.getPoint(strAddress, function(point){
-            console.log(point)
             if(point){
                 geoc.getLocation(point,function(result){
                     console.log(result);
 
                     $('#hiAddrId').attr("value",JSON.stringify(result.addressComponents));
-                    //var readAddrJson = jQuery.parseJSON($('#hiAddrId').attr("value"));
 
                 })
             } else {
-                console.log("无法解析当前地址");
-                alert("请输入详细有效的地址");
+                myCity.get(function(result){
+                    geoc.getLocation(result.center,function(result){
+                        $('#hiAddrId').attr("value",JSON.stringify(result.addressComponents));
+                    })
+                });
+                confirm("请输入详细有效的地址");
             }
         })
     }
@@ -42,10 +44,29 @@ function initialize(){
     map.disableDoubleClickZoom();
     map.clearOverlays();
 
-    var myCity = new BMap.LocalCity();
+
+
+    /**
+     * 载入时读取地图信息
+     */
+    (function readAddressLoc() {
+        if ( $('#hiAddrId').attr("value") ){
+            var readAddrJson = jQuery.parseJSON($('#hiAddrId').attr("value"));
+            var addrNodeBox = $(document.createElement("div"));
+            var addressNode = "<span>" + readAddrJson.province + "</span><span>" + readAddrJson.city + "</span><span>" + readAddrJson.district + "</span><span>" + readAddrJson.street + "</span><span>" + readAddrJson.streetNumber + "</span>"
+            var addPrintBox = $("#addPrintBox");
+            var addressText = $("#addressText");
+
+            addressText.html(addressNode);
+            addrNodeBox.html(addressNode);
+            addrNodeBox.addClass('myshop-addPrint d-inb');
+            addPrintBox.append(addrNodeBox);
+        }
+    })();
+
+
 
     map.addEventListener("click",setAddressPoint);
-
     function setAddressPoint (e){
         map.clearOverlays();
 
@@ -68,7 +89,6 @@ function initialize(){
                 "lat" : rs.point.lat
             };
 
-            //console.log(JSON.stringify(addJson));
             $('#hiAddrId').attr("value",JSON.stringify(addJson));
             var addressNode = "<span>" + addComp.province + "</span><span>" + addComp.city + "</span><span>" + addComp.district + "</span><span>" + addComp.street + "</span><span>" + addComp.streetNumber + "</span>"
             addressText.html(addressNode);
@@ -85,23 +105,6 @@ function initialize(){
 
     }
 
-    /**
-     * 载入时读取地图信息
-     */
-    (function readAddressLoc() {
-        if ( $('#hiAddrId').attr("value") ){
-            var readAddrJson = jQuery.parseJSON($('#hiAddrId').attr("value"));
-            var addrNodeBox = $(document.createElement("div"));
-            var addressNode = "<span>" + readAddrJson.province + "</span><span>" + readAddrJson.city + "</span><span>" + readAddrJson.district + "</span><span>" + readAddrJson.street + "</span><span>" + readAddrJson.streetNumber + "</span>"
-            var addPrintBox = $("#addPrintBox");
-            var addressText = $("#addressText");
-
-            addressText.html(addressNode);
-            addrNodeBox.html(addressNode);
-            addrNodeBox.addClass('myshop-addPrint d-inb');
-            addPrintBox.append(addrNodeBox);
-        }
-    })();
 
     /**
      * 打开地图时加载保存的数据
