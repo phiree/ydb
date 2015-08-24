57e4eafd6c5dd0b25e558a3e80ac2a29d4f04c31
phiree <phiree@gmail.com>
2015-08-24 16:31:22 +0800

    解决方案归类

---
+++ Dianzhu.Test/tagTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.BLL;
+ 
+using Dianzhu.Model;
+using NUnit.Framework;
+using FizzWare.NBuilder;
+using Rhino.Mocks;
+namespace Dianzhu.Test
+{
+    [TestFixture]
+  public  class tagTest
+    {
+        [Test]
+        public void test_create()
+        {
+            var dal = MockRepository.GenerateStub<DAL.DALDZTag>();
+
+            var cc = Builder<BLLDZTag>.CreateNew().WithConstructor(() => new BLLDZTag());
+
+        }
+    }
+}
--- Dianzhu.Test/test_mainform.cs
+++ Dianzhu.Test/test_mainform.cs
@@ -26,14 +26,25 @@ namespace Dianzhu.Test
         [Test]
         public void sdfsdfsd()
         {
+            DAL.DALMembership dalmember = Builder<DAL.DALMembership>.CreateNew().Build();
+            DAL.DALReception DALReception = Builder<DAL.DALReception>.CreateNew().Build();
+            DAL.DALDZService DALDZService = Builder<DAL.DALDZService>.CreateNew().Build();
             DZMembershipProvider bllMember = Builder<DZMembershipProvider>.CreateNew()
+                .With(x=>x.DALMembership=dalmember)
                      .Build();
 
-            BLLReception bllReception = Builder<BLLReception>.CreateNew().Build();
-            BLLDZService bllDZService = Builder<BLLDZService>.CreateNew().Build();
+            BLLReception bllReception = Builder<BLLReception>.CreateNew()
+             .With(x => x.DALReception = DALReception).Build();
+
+            BLLDZService bllDZService = Builder<BLLDZService>.CreateNew().
+                With(x => x.DALDZService = DALDZService).Build();
             MainFormView view = Builder<MainFormView>.CreateNew().Build();
-            FormController formController = new FormController(view, bllMember
-                 , bllReception, bllDZService);
+            FormController formController = new FormController(
+                view, 
+                bllMember
+                 , bllReception, 
+                 bllDZService,
+                 Builder<DZMembership>.CreateNew().Build());
             formController.ReceiveMessage("a@a.a", "hello", "/pic.png");
             string result = view.ToString();
             Assert.AreEqual("hello", result);
