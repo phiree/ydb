c0d109b1228ab7d9e0813f9452bc5760a4ebaa34
hanpengfei1 <609805657@qq.com>
2015-11-26 16:29:26 +0800

    更新全局变量

--- Dianzhu.CSClient.Presenter/GlobalViables.cs
+++ Dianzhu.CSClient.Presenter/GlobalViables.cs
@@ -9,16 +9,14 @@ namespace Dianzhu.CSClient.Presenter
 {
     
     public class GlobalViables
-    {  public static readonly string ButtonNamePrefix = "btn";
-
+    {
+        public static readonly string ButtonNamePrefix = "btn";
         
-        /// <summary>
-        /// 当前登录的客服
-        /// </summary>
          /// <summary>
          /// 当前登录的客服
          /// </summary>
          public static DZMembership CurrentCustomerService = null;
+        public static DZMembership Diandian = null;
         public static string MediaUploadUrl = ConfigurationManager.AppSettings["MediaUploadUrl"];// "http://192.168.1.140:8037/UploadFile.ashx";
         public static string MediaGetUrl = ConfigurationManager.AppSettings["MediaGetUrl"];//"http://192.168.1.140:8037/GetFile.ashx?fileName=";
         public static readonly string LocalMediaSaveDir =Environment.CurrentDirectory+ @"\localmedia\";
