ac38ec3d642251b4d48f34f52a369b73ac7e8c7b
hanpengfei1 <609805657@qq.com>
2015-12-16 16:51:30 +0800

    增加bll及单元测试

---
+++ Dianzhu.BLL/BLLReceptionChatDD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.DAL;
+using System.Device.Location;
+using Dianzhu.Model;
+using System.Web.Security;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using Dianzhu.Model;
+namespace Dianzhu.BLL
+{
+    /// <summary>
+    /// 接待记录.
+    /// </summary>
+    public class BLLReceptionChatDD
+    {
+        public DALReceptionChatDD DALReceptionChatDD = null;
+        public BLLReceptionChatDD() { DALReceptionChatDD = DALFactory.DALReceptionChatDD; }
+        public BLLReceptionChatDD(DALReceptionChatDD dalDD)
+        {
+            DALReceptionChatDD = dalDD;
+        }
+        public void Save(ReceptionChatDD chatDD)
+        {
+            DALReceptionChatDD.Save(chatDD);
+        }
+        
+        public bool FindChatByOrder(ServiceOrder order)
+        {
+            return DALReceptionChatDD.FindChatByOrder(order);
+        }
+    }
+
+}
---
+++ Dianzhu.DAL/DALReceptionChatDD.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.Model;
+using NHibernate;
+
+namespace Dianzhu.DAL
+{
+    public class DALReceptionChatDD : DALBase<Model.ReceptionChatDD>
+    {
+         public DALReceptionChatDD()
+        {
+             
+        }
+        //注入依赖,供测试使用;
+         public DALReceptionChatDD(string fortest):base(fortest)
+        {
+            
+        }
+
+        public new void Save(ReceptionChatDD chat)
+        {
+            if (chat.Id != Guid.Empty)
+            {
+                base.Save(chat, chat.Id);
+            }
+            else {
+                base.Save(chat);
+            }
+            //if (chat.Id != null)
+            //{ Session.Save(chat, chat.Id); }
+            //else {
+            //    Session.Save(chat);
+            //}
+            //return chat;
+        }
+
+        /// <summary>
+        /// 根据订单获取聊天记录
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public IList<ReceptionChatDD> GetChatListByOrder(ServiceOrder order)
+        {
+            var list = Session.QueryOver<ReceptionChatDD>().Where(x => x.ServiceOrder == order).OrderBy(x=>x.SavedTime).Desc.List();
+
+            return list;
+        }
+
+        public bool FindChatByOrder(ServiceOrder order)
+        {
+            ReceptionChat chat = null;
+
+            //var list = Session.QueryOver<ReceptionChatDD>()
+            //    .Right.JoinAlias(x => x.Reception, () => chat)
+            //    .Where(x => x.ServiceOrder == order)
+            //    .And(x => x.CopyFrom == null)
+            //    .List();
+
+            string sql = "SELECT rdd FROM ReceptionChatDD rdd" +
+                " RIGHT OUTER JOIN rdd.ReceptionChat r " +
+                " WHERE r.ServiceOrder_id = '" + order.Id + "'"+
+                " AND rdd.CopyFrom_id IS NULL";
+
+            //string sql = "select s from DZService s " +
+            //             //"  left join fetch s.Business b " +
+            //             //" inner join b.business_abs " +
+            //             //" inner join s.servicetype " +
+            //             " where s.Name like '%" + keywords + "%' ";
+
+            IQuery query = Session.CreateQuery(sql);
+            var list = query.List<ReceptionChatDD>();
+            if (list.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
