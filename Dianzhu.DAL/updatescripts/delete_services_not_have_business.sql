95615a6c01fb99b04675d33f738105ca3c032f66
phiree <phiree@gmail.com>
2016-01-05 17:18:04 +0800

    数据库整理脚本

--- Dianzhu.DAL/updatescripts/delete_services_not_have_business.sql
+++ Dianzhu.DAL/updatescripts/delete_services_not_have_business.sql
@@ -1,3 +1,51 @@
+/*
+说明:
+	删除无效外键数据, 防止 nhibernate 自动更新结构时抛出不可见异常,影响性能
+使用:
+	先手动选中 delete...语句,再执行.
+执行记录:
+
+	2016-1-5
+	2016-1-4
+
+*/
+
+SELECT *
+#delete CashTicketTemplate
+FROM CashTicketTemplate LEFT JOIN business_abs b ON business_id=b.`Id`
+WHERE b.id IS NULL
+ 
+
+SELECT *
+#delete businessuser
+FROM businessuser LEFT JOIN business_abs b ON belongto_id=b.`Id`
+WHERE b.id IS NULL;
+
+SELECT CashTicketCreateRecord_id,b.id,b.`Business_id`,c.`Id`
+#delete cashticket
+ FROM cashticket   INNER JOIN  CashTicketCreateRecord b ON  CashTicketCreateRecord_id= b.`Id`
+		   LEFT JOIN business_abs c ON  b.`Business_id`= c.id
+ WHERE c.id IS NULL
+ 
+
+SELECT *
+#delete CashTicketCreateRecord
+ FROM CashTicketCreateRecord   LEFT JOIN business_abs b ON  Business_id= b.id
+WHERE b.id IS NULL
+
+
+SELECT *
+#delete BusinessImage
+ FROM BusinessImage   LEFT JOIN business_abs b ON  Business_id= b.id
+WHERE b.id IS NULL
+
+
+SELECT *
+#delete staff
+ FROM staff   LEFT JOIN business_abs b ON `Belongto_id`=b.`Id`
+WHERE b.id IS NULL
+
+
 SELECT receptionchat_id
 #delete 
 FROM receptionchatreassign WHERE receptionchat_id IN ( 
