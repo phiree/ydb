bfffba414e40e7d74bdd5bfcd834b040598f066f
phiree <phiree@gmail.com>
2016-01-13 14:49:47 +0800

    封装http 请求

---
+++ PHSuit/HttpHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace PHSuit
+{
+    public class HttpHelper
+    {
+        public static string CreateHttpRequest(string url, string type, NameValueCollection paras)
+        {
+            var responseString = string.Empty;
+            using (var wb = new WebClient())
+            {
+                switch (type.ToLower())
+                {
+                    case "get":
+                        responseString = wb.DownloadString(url);
+                        break;
+                    case "post":
+                        byte[] response = wb.UploadValues(url, "POST", paras);
+                        responseString = Encoding.UTF8.GetString(response);
+                        break;
+                    default:
+                        throw new Exception("Unsupported method");
+
+                }
+
+            }
+
+            return responseString;
+
+        }
+    }
+}
