b8a13a6805906cd4e6b08875aee781a46822ba15
hanpengfei1 <609805657@qq.com>
2016-01-15 17:29:18 +0800

    1、新增广告类 2、新增订单状态更新历史记录表

---
+++ Dianzhu.DAL/DALAdvertisement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.Model;
+using NHibernate;
+
+namespace Dianzhu.DAL
+{
+    public class DALAdvertisement : DALBase<Advertisement>
+    {
+         public DALAdvertisement()
+        {
+             
+        }
+        //注入依赖,供测试使用;
+         public DALAdvertisement(string fortest):base(fortest)
+        {
+            
+        }
+        
+        public IList <Advertisement> GetADList()
+        {
+            return Session.QueryOver<Advertisement>().OrderBy(x => x.Num).Asc.List();
+        }
+    }
+}
---
+++ Dianzhu.HttpApi/App_Code/AD/AD001006.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dianzhu.BLL;
+using Dianzhu.Model;
+using System.Net;
+using Dianzhu.Model.Enums;
+/// <summary>
+/// Summary description for AD001006
+/// </summary>
+public class ResponseAD001006:BaseResponse
+{
+    BLLDeviceBind bllDeviceBind;
+
+    public ResponseAD001006(BaseRequest request):base(request)
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    protected override void BuildRespData()
+    {
+        try
+        {
+            BLLAdvertisement bllAD = new BLLAdvertisement();
+            IList<Advertisement> adList = bllAD.GetADList();
+            if (adList.Count > 0)
+            {
+                this.state_CODE = Dicts.StateCode[0];
+                return;
+            }
+            this.state_CODE = Dicts.StateCode[4];
+            this.err_Msg = "当前没有广告！";
+            return;
+        }
+        catch (Exception ex)
+        {
+            this.state_CODE = Dicts.StateCode[1];
+            this.err_Msg = ex.Message;
+            return;
+        }
+    }
+}
