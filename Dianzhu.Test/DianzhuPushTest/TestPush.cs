18eb4532a63f7a98528e97808d3b779042a7ff52
phiree@gmail.com <ochina1>
2016-01-20 17:55:56 +0800

    push

---
+++ Dianzhu.Test/DianzhuPushTest/TestPush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Dianzhu.Push;
+namespace Dianzhu.Test.DianzhuPushTest
+{
+    [TestFixture]
+    public class TestPush
+    {
+        [Test]
+        public void TestPushIOS()
+        {
+            //         Push.pushNotifications("8de76c196a605120db39ab58373edf159c1301b43659bd129fcf72b696e2a26c", "test_push", @"files\aps_development_Mark.p12", 1);
+
+            IPush ipush = new PushIOS("9192b3cc5112899606a2dbc5968ad948213d76ee73bf6ab3b3fa7c13ce0a58dd", 1, string.Empty);
+            ipush.Push("test_push" + DateTime.Now.ToString());
+        }
+        [Test]
+        public void TestPushIOSFromWeb()
+        {
+            PHSuit.HttpHelper.CreateHttpRequest(
+                string.Format("http://localhost:8040/push.ashx?client={0}&pushNum={1}&notificaitonSound={2}&message={3}&deviceToken={4}",
+                "ios",1,string.Empty,"i am pusing from web api, "+DateTime.Now.ToString(), "9192b3cc5112899606a2dbc5968ad948213d76ee73bf6ab3b3fa7c13ce0a58dd")
+                ,"get",null);
+                
+        }
+    }
+}
