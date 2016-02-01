a89e2b4a3e59db013d661d26159e4005252e1265
phiree <phiree@gmail.com>
2016-02-01 17:35:07 +0800

    1) 增加 支付项. 2) 增加 员工分配的定义 和框架代码

---
+++ Dianzhu.DAL/DALPayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dianzhu.Model;
+using NHibernate.Criterion;
+
+namespace Dianzhu.DAL
+{
+    public class DALPayment : DALBase<Model.Payment>
+    {
+         public DALPayment()
+        {
+             
+        }
+        //注入依赖,供测试使用;
+         public DALPayment(string fortest):base(fortest)
+        {
+            
+        }
+
+        /// <summary>
+        /// 获取一个订单已经
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public IList<Payment> GetPaymentsForOrder(ServiceOrder order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dianzhu.Model/OrderAssignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dianzhu.Model
+{
+    /// <summary>
+    /// 订单分配详情.
+    /// </summary>
+    public   class OrderAssignment
+    {
+        public virtual Guid Id { get; set; }
+        /// <summary>
+        /// 被分配的订单
+        /// </summary>
+        public virtual ServiceOrder Order { get; set; }
+        /// <summary>
+        /// 该订单分配的员工. 如果是多个员工,则创建多个对象.
+        /// </summary>
+        public virtual Staff AssignedStaff { get; set; }
+        /// <summary>
+        /// 分配的时间
+        /// </summary>
+        public virtual DateTime AssignedTime { get; set; }
+
+        /// <summary>
+        /// 取消分配的时间
+        /// </summary>
+        public virtual DateTime DeAssignedTime { get; set; }
+        /// <summary>
+        /// 是否有效. 否:已经取消了该分配
+        /// </summary>
+        public virtual bool Enabled { get; set; }
+    }
+}
