8a8a2af183e7858bdfa571c33ad29769c1a9b1db
phiree <phiree@gmail.com>
2016-03-08 09:13:19 +0800

    模拟即时通讯服务器,便于测试.

---
+++ InstantMessageMock/IMMock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dianzhu.CSClient.IInstantMessage;
+ 
+using Dianzhu.Model;
+namespace InstantMessageMock
+{
+    public class IMMock : InstantMessage
+    {
+        
+        public string Domain
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public string Server
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public event Dianzhu.CSClient.IInstantMessage.IMAuthError IMAuthError;
+        public event Dianzhu.CSClient.IInstantMessage.IMClosed IMClosed;
+        public event Dianzhu.CSClient.IInstantMessage.IMConnectionError IMConnectionError;
+        public event Dianzhu.CSClient.IInstantMessage.IMError IMError;
+        public event Dianzhu.CSClient.IInstantMessage.IMIQ IMIQ;
+        public event Dianzhu.CSClient.IInstantMessage.IMLogined IMLogined;
+        public event Dianzhu.CSClient.IInstantMessage.IMPresent IMPresent;
+        public event Dianzhu.CSClient.IInstantMessage.IMReceivedMessage IMReceivedMessage;
+        public event Dianzhu.CSClient.IInstantMessage.IMStreamError IMStreamError;
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenConnection(string userName, string password)
+        {
+             
+        }
+
+        public void SendMessage(string xml)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(Dianzhu.Model.ReceptionChat chat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendPresent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
