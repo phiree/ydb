6944d59ec6d5a26bc51b54b88f769b503b34c8c0
licdream <licdream@126.com>
2016-03-10 16:14:01 +0800

    接口适配器interfaceAdapter.js修改
    1.reqPackage只允许obj类型；
    2.添加请求错误代码反馈；

--- AdminBusiness/js/interfaceAdapter.js
+++ AdminBusiness/js/interfaceAdapter.js
@@ -43,17 +43,17 @@
                         if ( target === copy ){
                             continue;
                         }
-
-                        if ( copy && ( tools.isObject(copy) || (copyIsArray = tools.isArray(copy)) ) ) {
+                        //TODO : may not worked in IE
+                        if ( copy && ( (copyIsArray = tools.isArray(copy) ) || tools.isObject(copy)  ) ) {
                             if ( copyIsArray ){
                                 copyIsArray = false;
                                 clone = src && tools.isArray(src) ? src : [];
                             } else {
-                                clone = src && tools.isPlainObject(src) ? src : {};
+                                clone = src && tools.isObject(src) ? src : {};
                             }
 
                             target[ name ] = tools.extend(clone, copy);
-                        } else if ( copy !== undefined ){
+                        } else if ( typeof copy !== "undefined" ){
                             target[ name ] = copy;
                         }
 
@@ -84,32 +84,37 @@
      * @param data source data
      */
     Adapter.reqPackage = function( protocolCode, data ){
-        var result = {};
+        var reqObj = {}, reqData;
+        reqData = data;
 
         //接口号
-        result.protocol_CODE = protocolCode;
+        reqObj.protocol_CODE = protocolCode;
 
         //请求数据
-        result.ReqData = {};
+        reqObj.ReqData = {};
 
         //时间戳
-        result.stamp_TIMES = "";
+        reqObj.stamp_TIMES = "";
 
         //序列号
-        result.serial_NUMBER = "";
+        reqObj.serial_NUMBER = "";
 
         //客户端名称
-        result.appName = "adminBusiness";
+        reqObj.appName = "adminBusiness";
 
-        if ( data !== undefined ){
-            result.ReqData = tools.extend({}, data );
+        if ( typeof reqData !== "undefined" ){
+            if ( typeof reqData === "object" && reqData ){
+                reqObj.ReqData = tools.extend({}, data );
+            } else {
+                throw new Error('request data type error: no json type data');
+            }
         }
 
-        result.stamp_TIMES = createTime();
+        reqObj.stamp_TIMES = createTime();
 
-        result.serial_NUMBER = createSerial("009");
+        reqObj.serial_NUMBER = createSerial("009");
 
-        return result;
+        return JSON.stringify(reqObj);
     };
 
     /**
@@ -118,38 +123,66 @@
      * @returns {*}
      */
     Adapter.respUnpack = function(row){
-        var rowData, output;
+        var rowData, respObj;
         rowData = row;
-        output = {};
+        respObj = {};
+
+        var errorMap = {
+            "009000" : "正常",
+            "009001" : "未知数据类型",
+            "009002" : "数据库访问错误",
+            "009003" : "违反数据唯一性约束",
+            "009004" : "数据库访问返回值个数错误",
+            "009005" : "数据资源忙",
+            "009006" : "数据超出范围",
+            "009007" : "提交过于频繁",
+            "001001" : "用户认证错误",
+            "001002" : "用户密码错误",
+            "001003" : "密码错误次数超限被锁定",
+            "001004" : "外部系统IP被拒绝",
+            "001005" : "第三方登陆失效",
+            "001006" : "第三方登陆即将失效"
+        }
+
+        if ( !rowData.protocol_CODE ){
+            throw new Error("error protocol code");
+        } else {
+            respObj.protocolCode = rowData.protocol_CODE;
+        }
+
         if ( rowData == null ) {
             throw new Error("row data is null");
         }
 
         if ( typeof rowData === "string" ){
             try {
-                rowData = JSON.parse(rowData)
+                rowData = JSON.parse(rowData);
             } catch(error) {
                 throw new Error ( "parse error" + error );
             }
         }
 
         if ( typeof rowData !== "object" ){
-            throw new Error("type error: row data is typeof" + typeof rowData + ",it should be type of json");
+            throw new Error("type error: data is typeof" + typeof rowData + ",it should be type of json");
         }
 
-        if ( !rowData.respData ){
-            output.respData = rowData.RespData;
-        }
-
-        if ( !rowData.protocol_CODE ){
-            throw new Error("error protocol code");
+        if ( rowData.state_CODE !== "009000" ){
+            respObj.respCorrect = false;
+            throw new Error('ERROR_CODE: ' + rowData.state_CODE + '-' + errorMap[rowData.state_CODE] + ': ' + rowData.err_Msg );
         } else {
-            output.protocolCode = rowData.protocol_CODE;
+            respObj.respCorrect = true;
+            if ( typeof rowData.RespData === "object" && rowData.RespData ){
+                respObj.respData = rowData.RespData;
+
+                if ( respObj.respData.arrayData && tools.isArray(respObj.respData.arrayData) ) {
+                    respObj.hasArrayData = true;
+                }
+            }
         }
 
-        output.stateCode = rowData.state_CODE;
+        respObj.stateCode = rowData.state_CODE;
 
-        return output;
+        return respObj;
     };
 
     /**
