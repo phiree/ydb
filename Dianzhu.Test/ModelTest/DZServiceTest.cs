71398856370db5a830cd630cc1b5a63803c026b4
phiree <phiree@gmail.com>
2016-03-22 19:04:24 +0800

    服务项的单元测试

---
+++ Dianzhu.Test/ModelTest/DZServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Dianzhu.Model;
+using FizzWare.NBuilder;
+namespace Dianzhu.Test.ModelTest
+{
+    [TestFixture]
+   public class DZServiceTest
+    {
+        [Test]
+        public void TestChangeName()
+        {
+             
+        }
+    }
+}
