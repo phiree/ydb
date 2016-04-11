f3abab734483339c3f95c7ff0e0cea7b76b97208
phiree <phiree@gmail.com>
2016-04-11 10:20:10 +0800

    feat(整个项目): 配置文件快捷替换
    使用一个bat文件,快速替换数据库配置文件.

---
+++ copy_config.bat
@@ -0,0 +1,9 @@
+copy /Y connectionstrings.config .\adminbusiness\config\connectionstrings.config
+copy /Y connectionstrings.config .\adminweb\config\connectionstrings.config
+copy /Y connectionstrings.config .\dianzhu.httpapi\config\connectionstrings.config
+copy /Y connectionstrings.config .\dianzhu.web.notify\config\connectionstrings.config
+copy /Y connectionstrings.config .\dianzhu.web.pay\config\connectionstrings.config
+copy /Y connectionstrings.config .\dianzhu.csclient\config\connectionstrings.config
+copy /Y connectionstrings.config .\dianzhu.test\config\connectionstrings.config
+
+pause
