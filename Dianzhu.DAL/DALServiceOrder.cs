6eca24d25ce3d54ef5eee40a84c572d14364a841
hanpengfei1 <609805657@qq.com>
2016-05-06 14:50:08 +0800

    fix(订单): 修复查询未完成订单时没有数据的bug

--- Dianzhu.DAL/DALServiceOrder.cs
+++ Dianzhu.DAL/DALServiceOrder.cs
@@ -34,12 +34,12 @@ namespace Dianzhu.DAL
                 case enum_OrderSearchType.De:
                     iqueryover = iqueryover.Where(
                         x => x.OrderStatus == enum_OrderStatus.Finished
-                        && x.OrderStatus == enum_OrderStatus.Appraised
-                        && x.OrderStatus == enum_OrderStatus.EndWarranty
-                        && x.OrderStatus == enum_OrderStatus.EndCancel
-                        && x.OrderStatus == enum_OrderStatus.EndRefund
-                        && x.OrderStatus == enum_OrderStatus.EndIntervention
-                        && x.OrderStatus == enum_OrderStatus.ForceStop
+                        || x.OrderStatus == enum_OrderStatus.Appraised
+                        || x.OrderStatus == enum_OrderStatus.EndWarranty
+                        || x.OrderStatus == enum_OrderStatus.EndCancel
+                        || x.OrderStatus == enum_OrderStatus.EndRefund
+                        || x.OrderStatus == enum_OrderStatus.EndIntervention
+                        || x.OrderStatus == enum_OrderStatus.ForceStop
                         );
                     break;
                 case enum_OrderSearchType.Nt:
