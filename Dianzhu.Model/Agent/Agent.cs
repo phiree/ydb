e119689d05f51482d37d2aef7707bf62885e81ad
phiree <phiree@gmail.com>
2016-05-10 17:04:41 +0800

    feat(代理商): 代理商管理(待完成)
    增加map文件

--- Dianzhu.Model/Agent/Agent.cs
+++ Dianzhu.Model/Agent/Agent.cs
@@ -9,7 +9,7 @@ namespace Dianzhu.Model
     /// 代理商
     /// 代理商发展当地线下店铺
     /// </summary>
-    public class Agent
+    public class Agent:DZMembership
     {
         /// <summary>
         /// 代理商代理的区域
@@ -17,9 +17,9 @@ namespace Dianzhu.Model
         public Area Area { get; set; }
 
         /// <summary>
-        /// 发展的商家
+        /// 发展的商家(只要是同一地区的商家即属于该代理)
         /// </summary>
-        public IList<Business_Abs> Businesses { get; set; }
+        
         /// <summary>
         /// 分润
         /// </summary>
