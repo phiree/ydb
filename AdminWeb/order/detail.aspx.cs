7917427ef26d56f8e6845a64d71bf6959873a8ad
longhui <longhui>
2016-05-25 11:01:00 +0800

    fix(order.detail):更新订单详情

---
+++ AdminWeb/order/detail.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dianzhu.BLL;
+using Dianzhu.Model;
+using System.Data;
+
+public partial class order_detail : System.Web.UI.Page
+{
+    BLLServiceOrder bllServiceOrder = new BLLServiceOrder();
+    ServiceOrder serviceorder;
+
+    string strID;//链接字符串
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["ID"] == "" || Request.QueryString["ID"] == null)
+        {
+            Response.Redirect("index.aspx");
+        }
+        else
+        {
+            strID = Request.QueryString["ID"].ToString();
+        }
+        Guid id = Guid.Parse(strID);
+        serviceorder = bllServiceOrder.GetOne(id);
+        //Dianzhu.Model.Enums.enum_OrderStatus status = (Dianzhu.Model.Enums.enum_OrderStatus)Enum.Parse(typeof(Dianzhu.Model.Enums.enum_OrderStatus), Request.QueryString["status"].ToString());
+        //serviceorder = bllServiceOrder.GetOrderStatusPrevious(status).OrderByDescending(x => x.OrderCreated);
+
+        lblTitle.Text = serviceorder.Title;
+        lblServiceBusinessName.Text = serviceorder.ServiceBusinessName;
+        lblDescription.Text = serviceorder.Description;
+        lblCustomer.Text = serviceorder.Customer.UserName;
+        lblCustomerService.Text = serviceorder.CustomerService.UserName;
+        lblOrderCreated.Text = serviceorder.OrderCreated.ToString("yyyy-MM-dd HH:mm:ss");
+        lblOrderConfirmTime.Text = serviceorder.OrderConfirmTime.ToString("yyyy-MM-dd HH:mm:ss");
+        lblLatestOrderUpdated.Text = serviceorder.LatestOrderUpdated.ToString("yyyy-MM-dd HH:mm:ss");
+        lblOrderFinished.Text = serviceorder.OrderFinished.ToString("yyyy-MM-dd HH:mm:ss");
+        lblOrderServerStartTime.Text = serviceorder.OrderServerStartTime.ToString("yyyy-MM-dd HH:mm:ss");
+        lblOrderServerFinishedTime.Text = serviceorder.OrderServerFinishedTime.ToString("yyyy-MM-dd HH:mm:ss");
+        lblMemo.Text = serviceorder.Memo;
+        lblOrderStatus.Text = serviceorder.OrderStatus.ToString();
+        lblTargetAddress.Text = serviceorder.TargetAddress;
+        lblTargetTime.Text = serviceorder.TargetTime;
+        //lblStaff.Text = serviceorder.Staff.ToArray().ToString();
+        if (serviceorder.Staff.Count > 0)
+        {
+            foreach (Staff s in serviceorder.Staff)
+            {
+                lblStaff.Text = lblStaff.Text + s.Name + ",";
+            }
+            lblStaff.Text = lblStaff.Text.TrimEnd(',');
+        }
+        else
+        { lblStaff.Text = ""; }
+        lblUnitAmount.Text = serviceorder.UnitAmount.ToString();
+        lblOrderAmount.Text = serviceorder.OrderAmount.ToString();
+        lblDepositAmount.Text = serviceorder.DepositAmount.ToString();
+        lblNegotiateAmount.Text = serviceorder.NegotiateAmount.ToString();
+        BLLPayment bllPayment = new BLLPayment();
+        Payment payment = bllPayment.GetOne(id);
+        if (payment == null)
+        {
+            lblGetPayAmount.Text = "0";
+        }
+        else
+        {
+            lblGetPayAmount.Text = bllPayment.GetPayAmount(serviceorder, payment.PayTarget).ToString();
+        }
+        lblTitle.Text = serviceorder.Title;
+        if (lblOrderStatus.Text == "Finished")
+        {
+            Dianzhu.BLL.Finance.BLLServiceTypePoint bllServiceTypePoint = new Dianzhu.BLL.Finance.BLLServiceTypePoint();
+            Dianzhu.BLL.Finance.BalanceFlowService balanceService=new Dianzhu.BLL.Finance.BalanceFlowService ();
+            Dianzhu.BLL.Finance.BLLSharePoint bllSharePoint=new Dianzhu.BLL.Finance.BLLSharePoint ();
+            Dianzhu.BLL.Agent.AgentService agentService =new Dianzhu.BLL.Agent.AgentService();
+            Dianzhu.BLL.Finance.OrderShare os = new Dianzhu.BLL.Finance.OrderShare(bllServiceTypePoint, bllSharePoint, agentService, balanceService);
+            IList < Dianzhu.Model.Finance.BalanceFlow > shareFlow= os.Share(serviceorder);
+            foreach (Dianzhu.Model.Finance.BalanceFlow bf in shareFlow)
+            {
+                if (bf.Member == serviceorder.CustomerService)
+                {
+                    lblCustomerServiceShare.Text = bf.Amount.ToString();
+                }
+                else
+                {
+                    lblAgentShare.Text = bf.Amount.ToString();
+                }
+            }
+        }
+        try {
+            lblPlatformShare.Text = (decimal.Parse(lblGetPayAmount.Text) - decimal.Parse(lblCustomerServiceShare.Text) - decimal.Parse(lblAgentShare.Text)).ToString();
+        }
+        catch { }
+    }
+}
