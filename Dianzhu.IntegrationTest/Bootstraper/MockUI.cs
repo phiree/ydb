73eb62cfa5a3f7bc7155daada0ab671f2ef47583
phiree <phiree@gmail.com>
2016-05-27 18:31:38 +0800

    refactor(测试):集成测试
    添加集成测试.

---
+++ Dianzhu.IntegrationTest/Bootstraper/MockUI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Mocks;
+using Dianzhu.CSClient.IView;
+namespace Dianzhu.IntegrationTest.Bootstraper
+{
+    public class LoginView : ILoginForm
+    {
+        string userName;
+        string plainPassword;
+        public LoginView(string username,string plainpassword)
+        {
+            this.userName = username;
+            this.plainPassword = plainpassword;
+
+        }
+        public string ErrorMessage
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string FormText
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsLoginSuccess
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool LoginButtonEnabled
+        {
+            set
+            {
+                
+            }
+        }
+
+        public string LoginButtonText
+        {
+            set
+            {
+              
+            }
+        }
+
+        public string LoginMessage
+        {
+            set
+            {
+                 
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return plainPassword;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+        }
+
+        public event EventHandler TestClick;
+        public event ViewLogin ViewLogin;
+
+        public bool ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dianzhu.IntegrationTest/RecetionAssignTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Dianzhu.CSClient.Presenter;
+using Dianzhu.CSClient.IView;
+namespace Dianzhu.IntegrationTest
+{
+    [TestFixture]
+    public class RecetionAssignTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Bootstraper.Bootstrap.Boot();
+        }
+        public void BatchCSClientLogin()
+        { }
+        [Test]
+        public void OpenfireLogin()
+        {
+            LoginPresenter loginPresenter = Bootstraper.Bootstrap.Container.Resolve<LoginPresenter>();
+            loginPresenter.loginView_ViewLogin();
+        }
+    }
+}
