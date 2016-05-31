57072ddb23730c32547dcddfee8c16da6b8d2784
longhui <longhui>
2016-05-31 16:49:08 +0800

    feat(RestfulApi):新增认证方法

---
+++ Dianzhu.Web.RestfulApi/Providers/AuthorizationServerProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Dianzhu.Web.RestfulApi
+{
+    internal class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// 重写客户验证方法
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        /// <summary>
+        /// 验证用户名和密码发送到认证服务器的令牌的端点
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            //using (AuthRepository _repo = new AuthRepository())
+            //{
+            //    IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+            //    if (user == null)
+            //    {
+            //        context.SetError("invalid_grant", "username password not match");
+            //        return;
+            //    }
+            //}
+            //var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            //identity.AddClaim(new Claim("sub", context.UserName));
+            //identity.AddClaim(new Claim("role", "user"));
+            //context.Validated(identity);
+        }
+    }
+}
