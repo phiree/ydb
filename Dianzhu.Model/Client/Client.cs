8311386afbc7ee8cb3ea97fb0c6affcf82da8da1
longhui <longhui>
2016-06-02 08:57:46 +0800

    feat(RestfulApi):新增Client和RefreshToken模板

---
+++ Dianzhu.Model/Client/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dianzhu.Model
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public string Name { get; set; }
+        public string ApplicationType { get; set; }
+        public bool Active { get; set; }
+        public int RefreshTokenLifeTime { get; set; }
+        public string AllowedOrigin { get; set; }
+    }
+}
