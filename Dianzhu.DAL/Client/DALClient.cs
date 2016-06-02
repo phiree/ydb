6d1b28d99c42343d50a5817e464044c868c5d109
longhui <longhui>
2016-06-02 10:21:54 +0800

    feat(RestfulApi):添加Client和RefreshToken的DAL

--- Dianzhu.DAL/Client/DALClient.cs
+++ Dianzhu.DAL/Client/DALClient.cs
@@ -9,5 +9,34 @@ namespace Dianzhu.DAL.Client
     public class DALClient : DALBase<Model.Client>
     {
 
+        public DALClient()
+        {
+
+        }
+
+        /// <summary>
+        /// 注册客户端
+        /// </summary>
+        /// <param name="client"></param>
+        public void RegisterClient(Model.Client client)
+        {
+            Save(client);
+        }
+
+        /// <summary>
+        /// 根据Id获取客户端数据
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public Model.Client FindClient(string clientId)
+        {
+            Model.Client client = null;
+            IQuery query = Session.CreateQuery("select m from  Client as m where Id='" + clientId + "'");
+            Action a = () => { client = query.UniqueResult<Model.Client>(); };
+            TransactionCommit(a);
+            return client;
+        }
+
+
     }
 }
---
+++ Dianzhu.DAL/Client/DALRefreshToken.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+
+namespace Dianzhu.DAL.Client
+{
+    class DALRefreshToken : DALBase<Model.RefreshToken>
+    {
+        public DALRefreshToken()
+        {
+
+        }
+
+        /// <summary>
+        /// 添加新的RefreshToken
+        /// </summary>
+        /// <param name="token"></param>
+        public void AddRefreshToken(Model.RefreshToken token)
+        {
+            using (var x = Session.Transaction)
+            {
+                x.Begin();
+
+                //判断某客户端的某个用户是否已经生成RefreshToken,若存在就先删除、后添加
+                var existingToken = Session.QueryOver<Model.RefreshToken>().Where(r => r.Subject == token.Subject && r.ClientId == token.ClientId).SingleOrDefault();
+                if (existingToken != null)
+                {
+                    Session.Delete(existingToken);
+                }
+
+                Session.Save(token);
+                x.Commit();
+            }
+        }
+
+        /// <summary>
+        /// 根据Id删除RefreshToken
+        /// </summary>
+        /// <param name="refreshTokenId"></param>
+        public void RemoveRefreshToken(string refreshTokenId)
+        {
+            var existingToken = Session.QueryOver<Model.RefreshToken>().Where(r => r.Id == refreshTokenId).SingleOrDefault();
+            Delete(existingToken);
+        }
+
+        /// <summary>
+        /// 删除RefreshToken
+        /// </summary>
+        /// <param name="refreshtoken"></param>
+        public void RemoveRefreshToken(Model.RefreshToken refreshtoken)
+        {
+            Delete(refreshtoken);
+        }
+
+        /// <summary>
+        /// 根据Id获取RefreshToken
+        /// </summary>
+        /// <param name="refreshTokenId"></param>
+        /// <returns></returns>
+        public Model.RefreshToken FindRefreshToken(string refreshTokenId)
+        {
+            Model.RefreshToken refreshtoken = null;
+            IQuery query = Session.CreateQuery("select m from  RefreshToken as m where Id='" + refreshTokenId + "'");
+            Action a = () => { refreshtoken = query.UniqueResult<Model.RefreshToken>(); };
+            TransactionCommit(a);
+            return refreshtoken;
+        }
+
+        /// <summary>
+        /// 获取所有的RefreshToken
+        /// </summary>
+        /// <returns></returns>
+        public IList<Model.RefreshToken> GetAllRefreshTokens()
+        {
+            IQuery query = Session.CreateQuery("select r from RefreshToken r ");
+            return query.List<Model.RefreshToken>();
+        }
+    }
+}
