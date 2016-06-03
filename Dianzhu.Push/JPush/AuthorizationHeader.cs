25fc92258bb0dbb461656d6feedf6bfc203f8132
phiree <phiree@gmail.com>
2016-06-03 15:43:56 +0800

    feat(离线推送): 离线推送测试页面
    ios和android推送功能测试页面.

---
+++ Dianzhu.Push/JPush/AuthorizationHeader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dianzhu.Push.JPush
+{
+    public class AuthorizationHeader
+    {
+        private static string AuthKey = string.Empty;
+        public static string GenerateAuthorizationValue()
+        {
+            if (string.IsNullOrEmpty(AuthKey))
+
+            {
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(Config.AppKey + ":" + Config.MasterSecret);
+                AuthKey = System.Convert.ToBase64String(plainTextBytes);
+            }
+            return "Basic "+ AuthKey;
+        }
+    }
+}
--- Dianzhu.Push/JPush/JPush.cs
+++ Dianzhu.Push/JPush/JPush.cs
@@ -25,7 +25,9 @@ namespace Dianzhu.Push.JPush
         {
             JPushRequest req = JPushRequest.Create(new string[] { "android" }, new string[] { target }, message,"您有一条新消息", orderid);
 
-            string result=  PHSuit.HttpHelper.CreateHttpRequestPostXml(Config.APIUrl,req.ToJson());
+            System.Net.WebHeaderCollection headers = new System.Net.WebHeaderCollection();
+            headers.Add(System.Net.HttpRequestHeader.Authorization,AuthorizationHeader.GenerateAuthorizationValue());
+            string result=  PHSuit.HttpHelper.CreateHttpRequestPostXml(Config.APIUrl,req.ToJson(),headers);
             // Newtonsoft.Json.JsonConvert.DeserializeObject<JPushResponse>(result);
             //简单处理
             log.Debug("推送结果" + result);
