1538ee49bc918e6ebd5ecdf47addbc9de017e41e
phiree <phiree@gmail.com>
2016-06-12 19:13:39 +0800

    feat(单元测试): 加密接口单元测试.

---
+++ Dianzhu.Test/InfraStructureTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JSYK.Infrastructure;
+using NUnit.Framework;
+namespace Dianzhu.Test.InfraStructureTest
+{
+    [TestFixture]
+    public class EncryptServiceTest
+    {
+        [Test]
+        public void TestGetMd5()
+        {
+            EncryptService ens = new EncryptService();
+            string md5=    ens.GetMD5Hash("123456");
+            Console.WriteLine(md5);
+        }
+    }
+}
