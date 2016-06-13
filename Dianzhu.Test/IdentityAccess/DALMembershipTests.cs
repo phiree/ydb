44f40b48410082f1adb5e9ed3771a185835ae876
phiree <phiree@gmail.com>
2016-06-13 17:52:45 +0800

    chome(组件更新): 更新nuget包

---
+++ Dianzhu.Test/IdentityAccess/DALMembershipTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Dianzhu.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dianzhu.DAL;
+namespace Dianzhu.DAL.Tests
+{
+    [TestFixture()]
+    public class DALMembershipTests
+    {
+        [Test()]
+        public void GetMemberByWechatOpenIdTest()
+        {
+            DAL.DALMembership dal = new DALMembership();
+            dal.GetMemberByWechatOpenId("aa");
+        }
+    }
+}
