46071092b23a6ad9e3c0efd7a832e93a3773e8d8
licdream <licdream@126.com>
2016-07-15 10:25:33 +0800

    fix(browserDetect): 浏览器检查引用修改

--- AdminBusiness/browserDetective.aspx.cs
+++ AdminBusiness/browserDetective.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class browserDetective : BasePage
+public partial class browserDetective : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
