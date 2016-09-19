88cf858fc2f5625402d1b50cae8fb6b068e20c0b
hanpengfei1 <609805657@qq.com>
2016-09-19 09:27:03 +0800

    fix(http接口): 修复编译错误

--- Dianzhu.HttpApi/App_Code/ORM/ORM002003.cs
+++ Dianzhu.HttpApi/App_Code/ORM/ORM002003.cs
@@ -109,7 +109,7 @@ public class ResponseORM002003 : BaseResponse
 
                 //目前只返回商户jid
                 Guid targetId = order.Details[0].OriginalService.Business.Owner.Id;
-                type = enum_XmppResource.YDBan_Store.ToString();
+                string type = enum_XmppResource.YDBan_Store.ToString();
 
                 RespDataORM002003 respData = new RespDataORM002003();
                 RespDataORM_storeObj storeObj = new RespDataORM_storeObj().Adap(order.Details[0].OriginalService.Business);
