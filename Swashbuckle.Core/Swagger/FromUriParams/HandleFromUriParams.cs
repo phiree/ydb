5dc53a150cd826d5cd402d982f8b7d100986dfe2
longhui <longhui>
2016-09-22 17:55:45 +0800

    fix(RestfulApi):去掉swagger筛选器前面的对象名

--- Swashbuckle.Core/Swagger/FromUriParams/HandleFromUriParams.cs
+++ Swashbuckle.Core/Swagger/FromUriParams/HandleFromUriParams.cs
@@ -42,7 +42,7 @@ namespace Swashbuckle.Swagger.FromUriParams
                 var refSchema = schemaRegistry.GetOrRegister(type);
                 var schema = schemaRegistry.Definitions[refSchema.@ref.Replace("#/definitions/", "")];
 
-                var qualifier = string.IsNullOrEmpty(objectParam.name) ? "" : (objectParam.name + ".");
+                var qualifier = "";// string.IsNullOrEmpty(objectParam.name) ? "" : (objectParam.name + ".");
                 ExtractAndAddQueryParams(schema, qualifier, objectParam.required, schemaRegistry, operation.parameters);
                 operation.parameters.Remove(objectParam);
             }
