e05196c703e16b18a2f9178aaf073da5129e436d
hanpengfei1 <609805657@qq.com>
2016-10-10 11:38:52 +0800

    fix(客服工具): 发布版本到测试服务器

--- PHSuit/test.cs
+++ PHSuit/test.cs
@@ -41,7 +41,7 @@ namespace PHSuit
             Console.WriteLine("localhost_yuanfei" + ":" + PHSuit.Security.Encrypt("data source=localhost;uid=root;pwd=root;database=dianzhu", false));
             Console.WriteLine("150:" + PHSuit.Security.Encrypt("data source=192.168.1.150;uid=ydb;pwd=jsyk2016;database=dianzhu_test", false));
                Console.WriteLine("Aliyun_Official:" + PHSuit.Security.Encrypt("data source=business.ydban.cn;uid=jsyk2016;pwd=X*G7czoy6twAYIz7;port=4407;database=dianzhu", false));
-            Console.WriteLine("Aliyun_Test:" + PHSuit.Security.Encrypt("data source=112.74.198.215;uid=jsyk2016;pwd=IQDv$qefeqFWuq_L;port=4407;database=dianzhu_publish", false));
+            Console.WriteLine("Aliyun_Test:" + PHSuit.Security.Encrypt("data source=dev.ydban.cn;uid=jsyk2016;pwd=IQDv$qefeqFWuq_L;port=4407;database=dianzhu_publish", false));
         }
         [Test]
         public void TestIOSPush()
