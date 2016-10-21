fa1daab9fce0da7d0eb0bac4cec77dfb4f7c340e
phiree <phiree@gmail.com>
2016-10-21 15:09:34 +0800

    refactor(聊天接口): 增加聊天信息 应用服务接口.

---
+++ Ydb.InstantMessage/Application/IChatService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ydb.InstantMessage.DomainModel.Chat;
+namespace Ydb.InstantMessage.Application
+{
+    /// <summary>
+    /// 聊天信息接口
+    /// </summary>
+   public  interface IChatService
+    {
+        /// <summary>
+        /// 接收到即使信息
+        /// </summary>
+        /// <param name="textChat"></param>
+        void ReceiveMessage(ReceptionChat chat);
+        bool SendMessage(ReceptionChat chat);
+    }
+}
