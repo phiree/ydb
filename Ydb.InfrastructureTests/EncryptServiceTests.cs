829749da40f56066220939ed06081bab02496507
phiree <phiree@gmail.com>
2016-11-30 08:37:49 +0800

    refactor(加密服务): 允许添加不参加加密解密的前缀,方便阅读

---
+++ Ydb.InfrastructureTests/EncryptServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Ydb.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ydb.Infrastructure.Tests
+{
+    [TestFixture()]
+    public class EncryptServiceTests
+    {
+        [Test()]
+        public void EncryptTestWithPrefixComment()
+        {
+            var encryptService = new EncryptService();
+            string rawString = "150___server:localhost;pwd=p;uid=3";
+            string encrypted = encryptService.Encrypt(rawString, false);
+            Assert.IsTrue(encrypted.StartsWith("150___"));
+
+            string decrpyted = encryptService.Decrypt(encrypted, false);
+            Assert.IsTrue(decrpyted.StartsWith("server"));
+        }
+        [Test()]
+        public void EncryptTestWithoutPrefixComment()
+        {
+            var encryptService = new EncryptService();
+            string rawString = "server:localhost;pwd=p;uid=3";
+            string encrypted = encryptService.Encrypt(rawString, false);
+            Assert.IsFalse(encrypted.StartsWith("150____"));
+
+            string decrpyted = encryptService.Decrypt(encrypted, false);
+            Assert.IsTrue(decrpyted.StartsWith("server"));
+        }
+        [Test()]
+        public void EncryptTestWithEmptyPrefixComment()
+        {
+            var encryptService = new EncryptService();
+            string rawString = "___server:localhost;pwd=p;uid=3";
+            string encrypted = encryptService.Encrypt(rawString, false);
+            Assert.IsFalse(encrypted.StartsWith("___"));
+
+            string decrpyted = encryptService.Decrypt(encrypted, false);
+            Assert.IsTrue(decrpyted.StartsWith("server"));
+        }
+    }
+}
