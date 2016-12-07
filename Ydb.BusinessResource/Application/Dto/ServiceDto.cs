6182458b569e6883290165ba2540d55bf352f898
longphui <502647115@qq.com>
2016-12-07 16:44:19 +0800

    fix(RestfulApi):给ServiceDto添加AllowedPayType字段

--- Ydb.BusinessResource/Application/Dto/ServiceDto.cs
+++ Ydb.BusinessResource/Application/Dto/ServiceDto.cs
@@ -31,5 +31,8 @@ namespace Ydb.BusinessResource.Application
         public virtual string ServiceBusinessPhone { get; set; }
         public virtual bool IsForBusiness { get; set; }
 
+        public virtual string AllowedPayType { get; set; }
+        
+
     }
 }
