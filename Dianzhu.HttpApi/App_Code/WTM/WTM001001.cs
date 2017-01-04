31b18e3d8905062846c5c302f295a5cafed26088
phiree <phiree@gmail.com>
2017-01-04 15:53:59 +0800

    refactor(工作时间): 删除不需要的代码

--- Dianzhu.HttpApi/App_Code/WTM/WTM001001.cs
+++ Dianzhu.HttpApi/App_Code/WTM/WTM001001.cs
@@ -25,7 +25,9 @@ public class ResponseWTM001001 : BaseResponse
 {
     log4net.ILog ilog = log4net.LogManager.GetLogger("Dianzhu.HttpApi");
 
-    public ResponseWTM001001(BaseRequest request) : base(request) { }
+    public ResponseWTM001001(BaseRequest request) : base(request) {
+        
+    }
     protected override void BuildRespData()
     {
         ReqDataWTM001001 requestData = this.request.ReqData.ToObject<ReqDataWTM001001>();
@@ -52,163 +54,7 @@ public class ResponseWTM001001 : BaseResponse
             this.state_CODE = Dicts.StateCode[0];
             this.RespData = respData;
           ;
-            /*
-            if (workTimeObj.startTime == workTimeObj.endTime)
-            {
-                this.state_CODE = Dicts.StateCode[1];
-                this.err_Msg = "服务开始时间不能等于结束时间！";
-                return;
-            }
-
-            Guid userID,svcID;
-            bool isUserId = Guid.TryParse(raw_id, out userID);
-            if (!isUserId)
-            {
-                this.state_CODE = Dicts.StateCode[1];
-                this.err_Msg = "userId格式有误";
-                return;
-            }
-
-            bool isSvcId = Guid.TryParse(svc_id, out svcID);
-            if (!isSvcId)
-            {
-                this.state_CODE = Dicts.StateCode[1];
-                this.err_Msg = "svcId格式有误";
-                return;
-            }
-
-            MemberDto member = null;
-            if (request.NeedAuthenticate)
-            {                
-                bool validated = new Account(memberService).ValidateUser(userID, requestData.pWord, this, out member);
-                if (!validated)
-                {
-                    return;
-                } 
-            }
-            else
-            {
-                member = memberService.GetUserById(userID.ToString());
-                if (member == null)
-                {
-                    this.state_CODE = Dicts.StateCode[1];
-                    this.err_Msg = "不存在该商户！";
-                    return;
-                }
-            }
-            try
-            {
-                DZService service = dzServiceService.GetOne2(svcID);
-                if (service == null)
-                {
-                    this.state_CODE = Dicts.StateCode[1];
-                    this.err_Msg = "不存在该服务！";
-                    return;
-                }
-
-                if (service.Business.OwnerId != userID)
-                {
-                    this.state_CODE = Dicts.StateCode[1];
-                    this.err_Msg = "该商户没有该服务！";
-                    return;
-                }
-                
-                if (repeat != null)
-                {
-                    string[] repeatList = repeat.Split(',');
-                    for(int i = 0; i < repeatList.Count(); i++)
-                    {
-                        foreach(ServiceOpenTime sotObj in service.OpenTimes)
-                        {
-                            if (sotObj.DayOfWeek == StringToWeek(repeatList[i]))
-                            {
-                                ServiceOpenTimeForDay sotDay = new ServiceOpenTimeForDay();
-                                sotDay.MaxOrderForOpenTime = Int32.Parse(workTimeObj.maxOrder);
-                                sotDay.TimeStart = workTimeObj.startTime;
-                                sotDay.TimeEnd = workTimeObj.endTime;
-                                sotDay.Enabled = workTimeObj.open == "Y" ? true : false;
-                                sotDay.ServiceOpenTime = sotObj;
-                                sotObj.AddServicePeriod(sotDay);
-                                break;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    int week;
-                    try
-                    {
-                        week = Int32.Parse(workTimeObj.week);
-                    }
-                    catch (Exception e)
-                    {
-                        ilog.Error("week中有非法字符！");
-                        this.state_CODE = Dicts.StateCode[1];
-                        this.err_Msg = "week中有非法字符！";
-                        return;
-                    }
-                    if (Int32.Parse(workTimeObj.week) > 7 || Int32.Parse(workTimeObj.week) < 1)
-                    {
-                        this.state_CODE = Dicts.StateCode[1];
-                        this.err_Msg = "week有误！";
-                        return;
-                    }
-                    
-                    foreach (ServiceOpenTime sotObj in service.OpenTimes)
-                    {
-                        if (sotObj.DayOfWeek == StringToWeek(week.ToString()))
-                        {
-                            ServiceOpenTimeForDay sotDay = new ServiceOpenTimeForDay();
-                            sotDay.MaxOrderForOpenTime = Int32.Parse(workTimeObj.maxOrder);
-                            sotDay.TimeStart = workTimeObj.startTime;
-                            sotDay.TimeEnd = workTimeObj.endTime;
-                            sotDay.Enabled = workTimeObj.open == "Y" ? true : false;
-                            sotDay.ServiceOpenTime = sotObj;
-                            sotObj.AddServicePeriod(sotDay);
-                            break;
-                        }
-                    }
-                }
-
-                ValidationResult vResult = new ValidationResult();
-                dzServiceService.SaveOrUpdate(service, out vResult);
-                NHibernateUnitOfWork.UnitOfWork.Current.TransactionalFlush();
-
-                if (!vResult.IsValid)
-                {
-                    this.state_CODE = Dicts.StateCode[1];
-                    foreach (ValidationFailure vr in vResult.Errors)
-                    {
-                        this.err_Msg += vr.ErrorCode + ":" + vr.ErrorMessage + "\n";
-                    }
-                    return;
-                }
-
-                RespDataWTM001001 respData = new RespDataWTM001001();
-                respData.arrayData = new List<string>();
-                foreach (ServiceOpenTime sotObj in service.OpenTimes)
-                {
-                    foreach(ServiceOpenTimeForDay sotDayObj in sotObj.OpenTimeForDay)
-                    {
-                        if(sotDayObj.TimeStart== workTimeObj.startTime&&sotDayObj.TimeEnd== workTimeObj.endTime)
-                        {
-                            respData.arrayData.Add(sotDayObj.Id.ToString());
-                            break;
-                        }                        
-                    }
-                }
-                this.state_CODE = Dicts.StateCode[0];
-                this.RespData = respData;
-        }
-        catch (Exception ex)
-            {
-                ilog.Error(ex.Message);
-                this.state_CODE = Dicts.StateCode[2];
-                this.err_Msg = ex.Message;
-                return;
-            }
-            */
+           
         }
         catch (Exception e)
         {
