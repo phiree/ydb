a2dba9e01138f75717c03c04983c361490736f90
longphui <502647115@qq.com>
2017-02-23 15:55:06 +0800

    fix(AdminAgent):在客户端显示助理的验证统计

--- AdminAgent/Controllers/AgentCustomerServiceManageController.cs
+++ AdminAgent/Controllers/AgentCustomerServiceManageController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ydb.Membership.Application;
+using Ydb.Membership.Application.Dto;
+using Ydb.Common.Domain;
 
 namespace AdminAgent.Controllers
 {
     public class AgentCustomerServiceManageController : Controller
     {
+        IDZMembershipService dzMembershipService = Bootstrap.Container.Resolve<IDZMembershipService>();
+        IList<Area> areaList = new List<Area> { new Area { Id = 2445 }, new Area { Id = 2446 }, new Area { Id = 2447}, new Area { Id = 2448 }, new Area { Id = 2449}, new Area { Id = 2450 } };
+        MemberDto memberAgent = new MemberDto();
         public ActionResult assistant_validate()
         {
-            return View();
+            IDictionary<Enum_ValiedateCustomerServiceType, IList<DZMembershipCustomerServiceDto>> dicDto = dzMembershipService.GetVerifiedDZMembershipCustomerServiceByArea(areaList);
+            return View(dicDto);
         }
     }
 }
