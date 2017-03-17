966661fb720fe5fa14edda315d1d65798cc0056e
phiree <phiree@gmail.com>
2017-03-17 09:10:37 +0800

    fix(openfire):  存储过程修改, 增加一个方法
    自定义函数 Split_Str传入参数长度从255改为65536(236*256)
    增加一方法读取组用户

--- OpenfireExtension/Proc_AddUserToGroup.sql
+++ OpenfireExtension/Proc_AddUserToGroup.sql
@@ -1,5 +1,5 @@
 ﻿
-
+------------添加组和组内用户----------
 DROP PROCEDURE IF EXISTS AddUserToGroup;
 
 DELIMITER //
@@ -31,5 +31,23 @@ END
 DELIMITER ;
 
 -- userage
-CALL AddUserToGroup('group1','userid1,userid2');
+-- CALL AddUserToGroup('group1','userid1,userid2');
+
+-------------- split 函数 ----------------- 
+DELIMITER $$
+
+USE `openfire_3_10_3`$$
+
+DROP FUNCTION IF EXISTS `SPLIT_STR`$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `SPLIT_STR`(
+  X VARCHAR(60000),
+  delim VARCHAR(12),
+  pos INT
+) RETURNS VARCHAR(255) CHARSET utf8
+RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(X, delim, pos),
+       LENGTH(SUBSTRING_INDEX(X, delim, pos -1)) + 1),
+       delim, '')$$
+
+DELIMITER ;
 
--- OpenfireExtensionTests/OpenfireDbServiceTests.cs
+++ OpenfireExtensionTests/OpenfireDbServiceTests.cs
@@ -17,6 +17,16 @@ namespace OpenfireExtension.Tests
             Ydb.Infrastructure.EncryptService en = new Ydb.Infrastructure.EncryptService();
             OpenfireDbService dbservice = new OpenfireDbService(en);
             dbservice.AddUsersToGroup("u1,u2,u4", "gg1");
+            IList<string> useridList = new List<string> { "002c0c77-c2a0-4dba-930c-a6b000f80ceb","00c02067-4900-41b6-b3d4-a68100c47cb9", "015351d4-ba0a-41b2-bc5e-a6b400c11c26",
+                "0404c4f9-05bd-4a27-9203-a674010cfc36","044bf6a7-6d7a-42bc-a76a-a69000363cbd","076cbb88-594f-462c-ba9b-a6bb010a94d0" };
+            string userids = string.Join(",", useridList);
+            string groupname = "13bedbf7-c561-4bc5-ae64-a73800b40ba9";
+            dbservice.AddUsersToGroup(userids, groupname);
+
+            IList<string> useridsFromDb = dbservice.GetUsersInGroup(groupname);
+
+            Console.WriteLine(useridsFromDb.Count);
+            Assert.AreEqual(useridsFromDb.Count, userids.Split(',').Length);
         }
     }
 }
